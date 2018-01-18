95c04d5b55bd90ce532b93055b81375cbf58e546
Marina Zadoyanchuk <marina.zadoyanchuk@gmail.com>
2018-01-18 18:30:29 +0200

    init ui kit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const dependecies = require('./dependencies')
+const typography = require('./typography')
+const palette = require('./color-palette')
+
+module.exports = {dependecies, typography, palette}
