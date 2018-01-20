0fc2c6d4e3df63d59414ad86d352d6326866c47a
Mykola Melnychenko <mykola.melnichenko@gmail.com>
2018-01-20 15:51:18 +0200

    W/zapdos/pricing/dev (#33)
    * remove margin from desc when desc last elem
    
    * change text
    
    * remove margin in main plan
    
    * remove mb in last item list
    
    * add optional wrapper and top icon (#32)
    
    * add optional top icon
    
    * add optional wrapper and top icon

--- src/component.js
+++ src/component.js
@@ -9,6 +9,9 @@ class Block extends React.Component {
 
   getModifierValue = path => _.get(['modifier', path], this.props.$block)
 
+  getOptionValue = (path, defaultValue = false) =>
+    _.getOr(defaultValue, ['options', path], this.props.$block)
+
   collectionItem = planIndex => ({index, children, className}) => {
     const {components: {Text}, style} = this.props
     return (
@@ -19,12 +22,43 @@ class Block extends React.Component {
     )
   }
 
+  planHeader = (itemNumber) => {
+    const {components: {Text, Icon}, style} = this.props
+    return [
+      <h2 className={style.plan__title}>
+        <Text bind={`plan-title-${itemNumber}`} />
+      </h2>,
+      this.getModifierValue('plan-icon') && (
+        <div className={style['plan__icon-wrapper']}>
+          <Icon bind={`plan-icon-${itemNumber}`} />
+        </div>
+      ),
+      this.getModifierValue('plan-price') && (
+        <p className={style.plan__price}>
+          <strong className={style['plan__price-count']}>
+            <Text bind={`plan-price-count-${itemNumber}`} />
+          </strong>
+          {this.getModifierValue('plan-additional-info') && (
+            <span className={style['plan__price-text']}>
+              <Text bind={`plan-price-text-${itemNumber}`} />
+            </span>
+          )}
+        </p>
+      ),
+    ]
+  }
+
   render() {
     const {components: {Collection, Text, Button, Icon}, $block: {options}, style} = this.props
 
     return (
       <section className={style.section}>
         <div className={style.section__inner}>
+          {this.getModifierValue('top-icon') && (
+            <div className={style['top-icon-wrapper']}>
+              <Icon className={style['top-icon']} bind="topIcon" />
+            </div>
+          )}
           {this.getModifierValue('block-title') && (
             <h1 className={style.title}>
               <Text bind="title" />
@@ -37,28 +71,7 @@ class Block extends React.Component {
           )}
           <div className={style['plans-wrapper']}>
             <article className={style.plan}>
-              <h2 className={style.plan__title}>
-                <Text bind="plan-title-1" />
-              </h2>
-              {this.getModifierValue('plan-icon') && (
-                <div className={style['plan__icon-wrapper']}>
-                  <Icon
-                    bind="plan-icon-1"
-                  />
-                </div>
-              )}
-              {this.getModifierValue('plan-price') && (
-                <p className={style.plan__price}>
-                  <strong className={style['plan__price-count']}>
-                    <Text bind="plan-price-count-1" />
-                  </strong>
-                  {this.getModifierValue('plan-additional-info') && (
-                    <span className={style['plan__price-text']}>
-                      <Text bind="plan-price-text-1" />
-                    </span>
-                  )}
-                </p>
-              )}
+              {this.getOptionValue('price-wrapper') ? (<div className={style['price-wrapper']}>{this.planHeader('1')}</div>) : this.planHeader('1')}
               {this.getModifierValue('plan-description') && (
                 <Collection
                   className={style.plan__list}
@@ -80,28 +93,7 @@ class Block extends React.Component {
               )}
             </article>
             <article className={classNames(style.plan, style['plan--main'])}>
-              <h2 className={style.plan__title}>
-                <Text bind="plan-title-2" />
-              </h2>
-              {this.getModifierValue('plan-icon') && (
-                <div className={style['plan__icon-wrapper']}>
-                  <Icon
-                    bind="plan-icon-2"
-                  />
-                </div>
-              )}
-              {this.getModifierValue('plan-price') && (
-                <p className={style.plan__price}>
-                  <strong className={style['plan__price-count']}>
-                    <Text bind="plan-price-count-2" />
-                  </strong>
-                  {this.getModifierValue('plan-additional-info') && (
-                    <span className={style['plan__price-text']}>
-                      <Text bind="plan-price-text-2" />
-                    </span>
-                  )}
-                </p>
-              )}
+              {this.getOptionValue('price-wrapper') ? (<div className={style['price-wrapper']}>{this.planHeader('2')}</div>) : this.planHeader('2')}
               {this.getModifierValue('plan-description') && (
                 <Collection
                   className={style.plan__list}
@@ -123,28 +115,7 @@ class Block extends React.Component {
               )}
             </article>
             <article className={style.plan}>
-              <h2 className={style.plan__title}>
-                <Text bind="plan-title-3" />
-              </h2>
-              {this.getModifierValue('plan-icon') && (
-                <div className={style['plan__icon-wrapper']}>
-                  <Icon
-                    bind="plan-icon-3"
-                  />
-                </div>
-              )}
-              {this.getModifierValue('plan-price') && (
-                <p className={style.plan__price}>
-                  <strong className={style['plan__price-count']}>
-                    <Text bind="plan-price-count-3" />
-                  </strong>
-                  {this.getModifierValue('plan-additional-info') && (
-                    <span className={style['plan__price-text']}>
-                      <Text bind="plan-price-text-3" />
-                    </span>
-                  )}
-                </p>
-              )}
+              {this.getOptionValue('price-wrapper') ? (<div className={style['price-wrapper']}>{this.planHeader('3')}</div>) : this.planHeader('3')}
               {this.getModifierValue('plan-description') && (
                 <Collection
                   className={style.plan__list}
@@ -169,7 +140,11 @@ class Block extends React.Component {
           {this.getModifierValue('additional-btn') && (
             <div className={style['btns-group']}>
               <Button
-                className={classNames(style.button, style['button--secondary'], style['button--size-md'])}
+                className={classNames(
+                  style.button,
+                  style['button--secondary'],
+                  style['button--size-md'],
+                )}
                 bind="cta"
               />
             </div>
@@ -190,59 +165,64 @@ Block.defaultContent = {
     },
     {
       id: '51b10491-1f3f-4a36-ac3f-e295f010589a',
-      text: 'Unlimited Channels',
+      text: '3 features',
     },
     {
       id: '0e123e72-fccb-48d7-ba4f-5107126f7fd3',
-      text: '200k Messages / Day',
+      text: '10 requests per month',
     },
   ],
   'plan-list-2': [
     {
       id: '8dc2c1fe-f6ef-455d-bf2f-4ecd45772afb',
-      text: '100 Max Connections',
+      text: '120 Max Connections',
     },
     {
       id: '5adbf7f8-9c0e-4032-9d2d-297344657843',
-      text: 'Unlimited Channels',
+      text: '15 features',
     },
     {
       id: '5ee6b361-c602-410f-afe9-4fdc7f86c6bf',
-      text: '200k Messages / Day',
+      text: '200 requests per month',
     },
   ],
   'plan-list-3': [
     {
       id: '78417aad-0f0a-49ca-8c58-a50f1d837dfe',
-      text: '100 Max Connections',
+      text: '150 Max Connections',
     },
     {
       id: 'f7da9246-3adc-485e-ab25-5c792af0b036',
-      text: 'Unlimited Channels',
+      text: '30 features',
     },
     {
       id: 'b30486fc-e1a1-401a-b98c-ca5da7c2fffb',
-      text: '200k Messages / Day',
+      text: 'Unlimited requests',
     },
   ],
+  topIcon: {
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+  },
   title: 'Pricing',
   'plan-title-1': 'Basic',
   'plan-title-2': 'Premium',
   'plan-title-3': 'Pro',
   'plan-icon-1': {
     id: '12345',
-    svg: '<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 25 16\'><path d=\'M22.48 1.054L5.808 12.977 3.245 4.833l3.055 2.1c.437.297 1.02.262 1.416-.083l4.803-4.186 2.45 2.072 1.93-1.374L13.255.274c-.437-.366-1.076-.366-1.506.007L6.87 4.537 1.807 1.06C1.398.78.864.793.468 1.076c-.395.29-.562.8-.416 1.27l4.026 12.83c.11.35.39.628.736.745.118.042.243.062.368.062.236 0 .472-.076.673-.214L21.23 4.77l-2.102 8.18-3.415-2.356-1.986 1.423 5.456 3.758c.306.214.7.262 1.056.13.353-.13.616-.427.714-.786L24.25 2.283c.12-.463-.07-.96-.464-1.23-.396-.262-.91-.262-1.305 0z\'></path></svg>',
+    svg:
+      "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 25 16'><path d='M22.48 1.054L5.808 12.977 3.245 4.833l3.055 2.1c.437.297 1.02.262 1.416-.083l4.803-4.186 2.45 2.072 1.93-1.374L13.255.274c-.437-.366-1.076-.366-1.506.007L6.87 4.537 1.807 1.06C1.398.78.864.793.468 1.076c-.395.29-.562.8-.416 1.27l4.026 12.83c.11.35.39.628.736.745.118.042.243.062.368.062.236 0 .472-.076.673-.214L21.23 4.77l-2.102 8.18-3.415-2.356-1.986 1.423 5.456 3.758c.306.214.7.262 1.056.13.353-.13.616-.427.714-.786L24.25 2.283c.12-.463-.07-.96-.464-1.23-.396-.262-.91-.262-1.305 0z'></path></svg>",
   },
   'plan-icon-2': {
     id: '12345',
-    svg: '<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 25 16\'><path d=\'M22.48 1.054L5.808 12.977 3.245 4.833l3.055 2.1c.437.297 1.02.262 1.416-.083l4.803-4.186 2.45 2.072 1.93-1.374L13.255.274c-.437-.366-1.076-.366-1.506.007L6.87 4.537 1.807 1.06C1.398.78.864.793.468 1.076c-.395.29-.562.8-.416 1.27l4.026 12.83c.11.35.39.628.736.745.118.042.243.062.368.062.236 0 .472-.076.673-.214L21.23 4.77l-2.102 8.18-3.415-2.356-1.986 1.423 5.456 3.758c.306.214.7.262 1.056.13.353-.13.616-.427.714-.786L24.25 2.283c.12-.463-.07-.96-.464-1.23-.396-.262-.91-.262-1.305 0z\'></path></svg>',
+    svg:
+      "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 25 16'><path d='M22.48 1.054L5.808 12.977 3.245 4.833l3.055 2.1c.437.297 1.02.262 1.416-.083l4.803-4.186 2.45 2.072 1.93-1.374L13.255.274c-.437-.366-1.076-.366-1.506.007L6.87 4.537 1.807 1.06C1.398.78.864.793.468 1.076c-.395.29-.562.8-.416 1.27l4.026 12.83c.11.35.39.628.736.745.118.042.243.062.368.062.236 0 .472-.076.673-.214L21.23 4.77l-2.102 8.18-3.415-2.356-1.986 1.423 5.456 3.758c.306.214.7.262 1.056.13.353-.13.616-.427.714-.786L24.25 2.283c.12-.463-.07-.96-.464-1.23-.396-.262-.91-.262-1.305 0z'></path></svg>",
   },
   'plan-icon-3': {
     id: '12345',
-    svg: '<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 25 16\'><path d=\'M22.48 1.054L5.808 12.977 3.245 4.833l3.055 2.1c.437.297 1.02.262 1.416-.083l4.803-4.186 2.45 2.072 1.93-1.374L13.255.274c-.437-.366-1.076-.366-1.506.007L6.87 4.537 1.807 1.06C1.398.78.864.793.468 1.076c-.395.29-.562.8-.416 1.27l4.026 12.83c.11.35.39.628.736.745.118.042.243.062.368.062.236 0 .472-.076.673-.214L21.23 4.77l-2.102 8.18-3.415-2.356-1.986 1.423 5.456 3.758c.306.214.7.262 1.056.13.353-.13.616-.427.714-.786L24.25 2.283c.12-.463-.07-.96-.464-1.23-.396-.262-.91-.262-1.305 0z\'></path></svg>',
+    svg:
+      "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 25 16'><path d='M22.48 1.054L5.808 12.977 3.245 4.833l3.055 2.1c.437.297 1.02.262 1.416-.083l4.803-4.186 2.45 2.072 1.93-1.374L13.255.274c-.437-.366-1.076-.366-1.506.007L6.87 4.537 1.807 1.06C1.398.78.864.793.468 1.076c-.395.29-.562.8-.416 1.27l4.026 12.83c.11.35.39.628.736.745.118.042.243.062.368.062.236 0 .472-.076.673-.214L21.23 4.77l-2.102 8.18-3.415-2.356-1.986 1.423 5.456 3.758c.306.214.7.262 1.056.13.353-.13.616-.427.714-.786L24.25 2.283c.12-.463-.07-.96-.464-1.23-.396-.262-.91-.262-1.305 0z'></path></svg>",
   },
-  description:
-    'The French Revolution constituted for the conscience of the dominant aristocratic class a fall from innocence, and upturning of',
+  description: '100% money back guarantee. Cancel if you are not satisfied',
   'plan-price-text-1': 'For all period',
   'plan-price-text-2': 'per month',
   'plan-price-text-3': 'per month',
@@ -351,6 +331,12 @@ Block.modifierScheme = [
     type: 'checkbox',
     label: 'Package button',
     defaultValue: true,
+  },
+  {
+    id: 'top-icon',
+    type: 'hidden',
+    label: 'Top icon decorator',
+    defaultValue: false,
   },
 ]
 
