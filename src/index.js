f2865bae8bd3cde7cc991584989de40061d01091
Marina Zadoyanchuk <marina.zadoyanchuk@gmail.com>
2018-01-26 17:44:05 +0200

    remove index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const dependecies = require('./dependencies')
-const typography = require('./typography')
-const palette = require('./color-palette')
-
-module.exports = {dependecies, typography, palette}
