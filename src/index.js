5711d66178f6ff9c08adfe840fc5b20b749c5b58
max-sindi <sindm93@mail.ru>
2018-01-31 15:02:25 +0200

    done

--- src/index.js
+++ src/index.js
@@ -11,8 +11,61 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: 'Inspiration for your life.',
+  subtitle: 'I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.',
+  background: {
+    type: 'image',
+    src: 'cover.jpg',
+  },
 }
 
-ExtendedWireframe.modifierScheme = []
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Title description',
+    defaultValue: true,
+  },
+  {
+    id: 'primary-btn',
+    type: 'checkbox',
+    label: 'Primary button',
+    defaultValue: true,
+  },
+  {
+    id: 'secondary-btn',
+    type: 'checkbox',
+    label: 'Secondary button',
+    defaultValue: false,
+  },
+  {
+    type: 'separator',
+  },
+  {
+    id: 'align',
+    type: 'radio-button-group',
+    name: 'Aligning',
+    defaultValue: 'left',
+    children: [
+      {
+        id: 'left',
+        label: 'left',
+      },
+      {
+        id: 'center',
+        label: 'center',
+      },
+      {
+        id: 'right',
+        label: 'right',
+      },
+    ],
+  },
+  {
+    id: 'caption-decorator',
+    type: 'hidden',
+    name: 'top decorator',
+    defaultValue: true,
+  }]
 
 export default ExtendedWireframe
