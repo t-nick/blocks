a1bae9b1b6a822040934eb10a1e4b0ff8cb52ca3
mharyst <mykhailo.haryst@gmail.com>
2018-01-31 17:12:28 +0200

    fix text

--- src/component.js
+++ src/component.js
@@ -132,7 +132,7 @@ Block.defaultContent = {
   heading: 'HEADING',
   subHeading: 'SUB HEADING',
   text: 'TEXT',
-  linkText: 'Lorem ipsum <a href="https://google.com">dolor sit amet,</a> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  linkText: 'Lorem ipsum <a href="https://google.com">dolor sit amet,</a> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
   smallText: 'SMALL TEXT',
   quote: 'QUOTE',
   caption: 'CAPTION',
