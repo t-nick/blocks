2f7be0e1dd78ac64e1af037fd2089a4978d80efd
alex_diachenko <diachenko_work@mail.ru>
2018-02-05 21:47:00 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -11,8 +11,129 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+
+  background: {
+    type: 'color',
+    color: '#0c1526',
+  },
+  projects: [
+    {
+      id: 'e9ec34d7-3cc6-49f3-a9ad-6ea69f59409c',
+      title: 'Founder',
+      subtitle: 'Chris Pippen',
+      text:
+            'After almost a decade spent as a deck master on a cargo ocean shipping line, Chris decided to finally do what he loved the most - painting art on other people.',
+      picture: {
+        resourceRef: '2.jpg',
+        alt: 'Project 1',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      id: '9310272a-1fa2-4876-b1db-a879bbd4bc52',
+      title: 'Co-Founder',
+      subtitle: 'Karin Pippen',
+      text:
+            "Chris's wife, Karin spent almost 12 years in law enforcement and DEA, but eventually quit to go full-time with her hobby - namely the art of piercing.",
+      picture: {
+        resourceRef: '1.jpg',
+        alt: 'Project 2',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+  ],
+  topIcon: {
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
+  title: 'Our team',
+  subtitle:
+'Each month, we usefully close our projects. Here is the list of our recent ones:',
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+  },
 }
 
-ExtendedWireframe.modifierScheme = []
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'title',
+    type: 'checkbox',
+    label: 'Block title',
+    defaultValue: true,
+  },
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Title description',
+    defaultValue: false,
+  },
+  {
+    id: 'block-button',
+    type: 'checkbox',
+    label: 'Button',
+    defaultValue: true,
+  },
+  {
+    id: 'project-category',
+    type: 'checkbox',
+    label: 'Project category',
+    defaultValue: true,
+  },
+  {
+    id: 'project-description',
+    type: 'checkbox',
+    label: 'Project description',
+    defaultValue: true,
+  },
+  {
+    id: 'project-button',
+    type: 'checkbox',
+    label: 'Project button',
+    defaultValue: true,
+  },
+  {
+    id: 'top-icon',
+    type: 'hidden',
+    label: 'Top icon decorator',
+    defaultValue: false,
+  },
+]
 
 export default ExtendedWireframe
