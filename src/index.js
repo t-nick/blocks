4c883d9f660c4f383a90e90f1724d72c5539fb9f
JulBeet <julbeet@gmail.com>
2018-02-05 20:22:58 +0200

    Design markup added

--- src/index.js
+++ src/index.js
@@ -11,8 +11,89 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  theme: 'dark',
+  title: 'Chris Pippen',
+  subtitle: 'Founder: Japanese, Neo-Traditional Style',
+  picture: {
+    src: 'Depositphotos_9434210_original.jpg',
+    alt: 'Chris Pippen',
+  },
+  socialIcons: {
+    networks: [
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'youtube',
+        name: 'YouTube',
+        url: 'http://youtube.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 0,
+      color: '#2d3646',
+      sizes: [10, 20, 30, 35, 40],
+      size: '35px',
+    },
+  },
+  text: 'Growing up in a family of sailors in North Carolina, Chris has been exposed to the art of tattoos his whole life. Naturally, he chose it as his day job!',
+
 }
 
-ExtendedWireframe.modifierScheme = []
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'title',
+    type: 'checkbox',
+    label: 'Block title',
+    defaultValue: true,
+  },
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Subtitle',
+    defaultValue: false,
+  },
+  {
+    id: 'text',
+    type: 'checkbox',
+    label: 'Company main text',
+    defaultValue: true,
+  },
+  {
+    id: 'socialIcons',
+    type: 'checkbox',
+    label: 'Social media buttons',
+    defaultValue: false,
+  },
+  {
+    id: 'button',
+    type: 'checkbox',
+    label: 'Button',
+    defaultValue: true,
+  },
+  {
+    id: 'top-icon',
+    type: 'hidden',
+    label: 'Top icon decorator',
+    defaultValue: false,
+  },
+  {
+    id: 'article-picture',
+    type: 'hidden',
+    label: 'Article picture',
+    defaultValue: true,
+  },
+]
 
 export default ExtendedWireframe
