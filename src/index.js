b9f37ec384f4cb384582b47aa9fee194f740cc09
KonstantinKH <kkhomenko9@gmail.com git config --global user.nameKonstantin>
2018-02-05 22:16:27 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -11,8 +11,40 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#0c1526',
+  },
+  theme: 'dark',
+  numbers: [
+    {
+      title: 'Rate',
+      label: 'for our customers',
+      value: '$250/hour',
+      id: '554d7318-3c82-4dd4-87e9-b0b92299199f',
+    },
+    {
+      title: 'minimal charge',
+      label: 'of our clients',
+      value: '$250',
+      id: 'aa002eee-2755-4850-b18a-c990cfa03e37',
+    },
+    {
+      title: 'full day session',
+      label: 'of our users',
+      value: '$1200',
+      id: '84957801-e554-42e1-ab7b-323f483e3f81',
+    },
+  ],
 }
 
-ExtendedWireframe.modifierScheme = []
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'number-title',
+    type: 'checkbox',
+    label: 'Additional information on numbers',
+    defaultValue: true,
+  },
+]
 
 export default ExtendedWireframe
