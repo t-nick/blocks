a3afebf1a072993de582319ee4ca87cdc6ec5fb1
nataliadiadkova <nataliadiadkova@gmail.com>
2018-02-06 02:28:06 +0200

    design kadabra blog_4_blog_item

--- src/index.js
+++ src/index.js
@@ -9,4 +9,110 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  theme: 'dark',
+  title: 'Related posts',
+  blog: [
+    {
+      id: '21a46d6e-0354-4a96-9af0-928ddae32095',
+      title: 'Is Piercing Safe for Your Health?',
+      description:
+        'As far as we are concerned, this is probably the most popular…',
+      category: 'September 11, 2017',
+      date: 'Keely Rutherford',
+      picture: {
+        resourceRef: 'blog_4_blog_item_img1.png',
+        alt: 'Article illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Continue Reading >',
+        type: 'link',
+      },
+    },
+    {
+      id: '41db0c4f-46b3-4835-8e77-062de63d018a',
+      title: 'The Best Botanical Tattoos on Instagram',
+      description:
+        'It’s been scientifically proven that being around plants improves…',
+      category: 'September 08, 2017',
+      date: 'Mike Tattooer',
+      picture: {
+        resourceRef: 'blog_4_blog_item_img2.png',
+        alt: 'Article illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Continue Reading >',
+        type: 'link',
+      },
+    },
+  ],
+}
+
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'post-publish-date',
+    type: 'checkbox',
+    label: 'Date of publishing',
+    defaultValue: true,
+  },
+  {
+    id: 'post-image',
+    type: 'checkbox',
+    label: 'Post image',
+    defaultValue: true,
+  },
+  {
+    id: 'post-description',
+    type: 'checkbox',
+    label: 'Post main text',
+    defaultValue: true,
+  },
+  {
+    id: 'post-link',
+    type: 'checkbox',
+    label: 'Post link',
+    defaultValue: true,
+  },
+  {
+    id: 'block-button',
+    type: 'checkbox',
+    label: 'Button',
+    defaultValue: false,
+  },
+  {
+    id: 'subtitle',
+    type: 'hidden',
+    label: 'Subtitle',
+    defaultValue: false,
+  },
+  {
+    id: 'top-icon',
+    type: 'hidden',
+    label: 'Top icon decorator',
+    defaultValue: false,
+  },
+]
+
 export default ExtendedWireframe
