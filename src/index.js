36d31266f90252fc995f68b5fe4b9258fe14d0eb
Administrator <alexandr_bondarchuk@mail.ru>
2018-02-06 07:55:57 +0200

    Changed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,48 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'cover_home.png',
+  },
+  title: 'Wanna Get Inked Or Pierced?<br> We’re The Place To Go To!',
+  subtitle: '10000 Happy Clients and Counting',
+  'cta-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+    size: 'lg',
+  },
+  'cta-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+    size: 'lg',
+  },
+  topCaption: 'welcome',
 }
 
 ExtendedWireframe.modifierScheme = []
