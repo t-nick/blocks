33bcf6f20e762f9699c6f237accd19d2e8e6d324
VladReznik <vlad_escape@ukr.net>
2018-02-06 11:55:26 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -21,6 +21,40 @@ ExtendedWireframe.defaultContent = {
         src: 'img-1.png',
         alt: 'Blake photo',
       },
+      socialIcons: {
+        networks: [
+          {
+            id: 'instagram',
+            name: 'instagram',
+            url: 'https://facebook.com/',
+          },
+          {
+            id: 'facebook',
+            name: 'facebook',
+            url: 'https://twitter.com/',
+          },
+          {
+            id: 'twitter',
+            name: 'twitter',
+            url: 'https://www.linkedin.com/',
+          },
+          {
+            id: 'linkedin',
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/',
+          },
+        ],
+        target: '_blank',
+        design: {
+          border: 'circle',
+          innerFill: true,
+          preset: 'preset001',
+          offset: 15,
+          color: '#151f30',
+          sizes: [15, 25, 35, 45],
+          size: '30px',
+        },
+      },
     },
     {
       id: 'd7b368a3-75c8-4039-80ef-15c783140026',
@@ -30,6 +64,35 @@ ExtendedWireframe.defaultContent = {
         src: 'img-2.png',
         alt: 'Ann Maisner photo',
       },
+      socialIcons: {
+        networks: [
+          {
+            id: 'facebook',
+            name: 'Facebook',
+            url: 'https://facebook.com/',
+          },
+          {
+            id: 'twitter',
+            name: 'Twitter',
+            url: 'https://twitter.com/',
+          },
+          {
+            id: 'linkedin',
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/',
+          },
+        ],
+        target: '_blank',
+        design: {
+          border: 'circle',
+          innerFill: true,
+          preset: 'preset001',
+          offset: 15,
+          color: '#151f30',
+          sizes: [15, 25, 35, 45],
+          size: '30px',
+        },
+      },
     },
     {
       id: 'b29ef90b-8d5c-4fd9-b6e4-64e248da29db',
@@ -38,6 +101,35 @@ ExtendedWireframe.defaultContent = {
       picture: {
         src: 'img-3.png',
         alt: 'Tomas Abbar photo',
+      },
+      socialIcons: {
+        networks: [
+          {
+            id: 'facebook',
+            name: 'Facebook',
+            url: 'https://facebook.com/',
+          },
+          {
+            id: 'twitter',
+            name: 'Twitter',
+            url: 'https://twitter.com/',
+          },
+          {
+            id: 'linkedin',
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/',
+          },
+        ],
+        target: '_blank',
+        design: {
+          border: 'circle',
+          innerFill: true,
+          preset: 'preset001',
+          offset: 15,
+          color: '#151f30',
+          sizes: [15, 25, 35, 45],
+          size: '30px',
+        },
       },
     },
   ],
