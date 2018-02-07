960eb74eaf2e8d86814286a9360a8f7058a834b9
max-sindi <britva.okkamaa@gmail.com>
2018-02-07 10:29:11 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-zapdos-gallery/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,6 +11,130 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  theme: 'dark',
+  title: 'Works',
+  gallery: [
+    {
+      title: 'Quantum CEO & our partners',
+      picture: {
+        resourceRef: 'item-1.png',
+        alt: 'Quantum CEO & our partners',
+        galleryId: 'gallery',
+      },
+      caption: 'Black tattoo in a traditional style by Brucius',
+      id: 'ce744570-02be-4080-b3f2-740d628ccb52',
+    },
+    {
+      title: 'Digital Marketing workshop lecturer',
+      picture: {
+        resourceRef: 'item-2.png',
+        alt: 'Digital Marketing workshop lecturer',
+        galleryId: 'gallery',
+      },
+      caption: 'Black  and Red  tattoo by John Paul',
+      id: 'a980742f-2b78-40b0-9fed-247e71056797',
+    },
+    {
+      title: 'Quantum Company at WebSummit',
+      picture: {
+        resourceRef: 'item-3.png',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+      caption: 'Black and Color roses & butterflies by Dani Cobra',
+      id: 'cb6bbbcc-1980-4575-af5c-0f46e33d6d1e',
+    },
+    {
+      title: 'UX workshop attendees',
+      picture: {
+        resourceRef: 'item-4.png',
+        alt: 'UX workshop attendees',
+        galleryId: 'gallery',
+      },
+      caption: 'Color tattoo in a neo traditional style by Leah',
+      id: '3e7b03a6-54fb-4d5c-8735-ddfd27f0c8fa',
+    },
+    {
+      title: 'UX workshop lecturer',
+      picture: {
+        resourceRef: 'item-5.png',
+        alt: 'UX workshop lecturer',
+        galleryId: 'gallery',
+      },
+      caption: 'Nose piercing by Manu',
+      id: 'd6741c92-36d7-4495-93cc-ba77ce79f2bb',
+    },
+    {
+      title: 'Quantum team',
+      picture: {
+        resourceRef: 'item-6.png',
+        alt: 'Quantum team',
+        galleryId: 'gallery',
+      },
+      caption: 'Big color rose by Man',
+      id: '529f880a-60f0-4c8f-9fc3-8c9f482051bb',
+    },
+    {
+      title: 'Quantum team',
+      picture: {
+        resourceRef: 'item-7.png',
+        alt: 'Quantum team',
+        galleryId: 'gallery',
+      },
+      caption: 'Custom tattoo design by Javier Rodríguez',
+      id: '529f880a-60f0-4c8f-9fc3-8c9f482051ba',
+    },
+    {
+      title: 'Quantum team',
+      picture: {
+        resourceRef: 'item-8.png',
+        alt: 'Quantum team',
+        galleryId: 'gallery',
+      },
+      caption: 'Tribal and Maori influenced design by Rafa Serrano',
+      id: '529f880a-60f0-4c8f-9fc3-8c9f482051bc',
+    },
+    {
+      title: 'Quantum team',
+      picture: {
+        resourceRef: 'item-9.png',
+        alt: 'Quantum team',
+        galleryId: 'gallery',
+      },
+      caption: 'Ears pierced by Kyle',
+      id: '529f880a-60f0-4c8f-9fc3-8c9f482051bd',
+    },
+    {
+      title: 'Quantum team',
+      picture: {
+        resourceRef: 'item-10.png',
+        alt: 'Quantum team',
+        galleryId: 'gallery',
+      },
+      caption: 'Eyebrows pierced by John Paul',
+      id: '529f880a-60f0-4c8f-9fc3-8c9f482051be',
+    },
+    {
+      title: 'Quantum team',
+      picture: {
+        resourceRef: 'item-11.png',
+        alt: 'Quantum team',
+        galleryId: 'gallery',
+      },
+      caption: 'Lips pierced by Dani Cobra',
+      id: '529f880a-60f0-4c8f-9fc3-8c9f482051bf',
+    },
+    {
+      title: 'Quantum team',
+      picture: {
+        resourceRef: 'item-12.png',
+        alt: 'Quantum team',
+        galleryId: 'gallery',
+      },
+      caption: 'Tongue pierced by Leah',
+      id: '529f880a-60f0-4c8f-9fc3-8c9f482051bg',
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = []
