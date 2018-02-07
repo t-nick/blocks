2493eb19b56de758af5e2a11300a97999597621e
Kirill Donenko <kirill.donenko@gmail.com>
2018-02-07 18:21:40 +0200

    main blocks

--- src/index.js
+++ src/index.js
@@ -11,10 +11,151 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      id: '900aa334-14f2-4c05-b078-78099a5725e5',
+      title: 'Organic cotton leggins',
+      description:
+        'Multipurpose assistance for online stores and offline retail businesses.',
+      picture: {
+        src: 'foto-1.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      id: 'cfb2c56d-7708-4459-bdf2-6780f4ba62b9',
+      title: 'ATTA cotton T-shirt',
+      description:
+        'Developing marketing and business strategy to help company grow fast.',
+      picture: {
+        src: 'foto-2.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      id: 'eea21546-b56e-406b-8cba-18a5a5ec1820',
+      title: 'ATTA hammock belt',
+      description:
+        'Building brand awareness for micro and small businesses. ',
+      picture: {
+        src: 'foto-3.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      id: 'aa5d3a04-43b7-4bb3-9c7a-00e1986f359e',
+      title: 'Service 4',
+      description:
+        'Aftersales support in marketing, sales, and staff training.',
+      picture: {
+        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+  ],
+  topIcon: {
+    svg:
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
+  title: 'Shop',
+  description:
+    'We deliver all kind of services that support small and micro businesses. Here are some of them:',
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+  },
+  'button-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  body: {defaultValue: true, label: 'Service main text', type: 'checkbox'},
+  'button-primary': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
+  'button-secondary': {defaultValue: true, label: 'Button', type: 'checkbox'},
+  heading: {defaultValue: true, label: 'Service title', type: 'checkbox'},
+  link: {defaultValue: false, label: 'Link', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Services description', type: 'checkbox'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
