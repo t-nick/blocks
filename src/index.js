c59a8472e463f2abff0baf9882ae35e8ea7af486
Vladimir Zelik <kazantip81v@gmail.com>
2018-02-08 19:20:58 +0200

    add content & style

--- src/index.js
+++ src/index.js
@@ -11,10 +11,28 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  theme: 'dark',
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'image-bg.jpg',
+    overlay: {
+      type: 'color',
+      color: 'var(--ui-dark-shade-color)',
+      opacity: 0.2,
+    },
+  },
+  blockquote:
+  'The most difficult thing is the decision to act, the rest is merely tenacity. The fears are paper tigers. You can do anything you decide to do. You can act to change and control your life; and the procedure, the process is its own reward.',
+  author: 'Amelia Earhart',
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  author: {defaultValue: true, label: 'Quote author', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
