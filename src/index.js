ce644ae0d840fb1e0b8aa9708f270a585fa5cfdb
nataliadiadkova <nataliadiadkova@gmail.com>
2018-02-08 23:43:06 +0200

    design seadra cta

--- src/index.js
+++ src/index.js
@@ -11,10 +11,40 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  theme: 'dark',
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'background.png',
+  },
+  title: 'Fast Delivery <p>Enjoy our free, fast, and reliable delivery service.</p>',
+  description:
+    '',
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'read more',
+    type: 'secondary',
+    size: 'md',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  picture: {defaultValue: false, label: 'Picture', type: 'hidden'},
+  subtitle: {defaultValue: false, label: 'Title description', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
