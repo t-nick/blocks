5d7863a39762087aea3dcf160bc49b7e3451f27c
JulBeet <julbeet@gmail.com>
2018-02-08 17:31:41 +0200

    gesign markup added

--- src/index.js
+++ src/index.js
@@ -11,10 +11,39 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'image.jpg',
+  },
+  theme: 'dark',
+  title: 'sanwich day <br/> $3.99',
+  description: 'Sanwich Salad Drink',
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'order online',
+    type: 'primary',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  picture: {defaultValue: false, label: 'Picture', type: 'hidden'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
