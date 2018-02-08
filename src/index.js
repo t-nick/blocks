1cc3f684eed4e1418833e4d92ee529c1a45d525a
alex_diachenko <diachenko_work@mail.ru>
2018-02-08 19:22:43 +0200

    first commit fix style

--- src/index.js
+++ src/index.js
@@ -11,10 +11,25 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'gradient',
+    gradient: {
+      angle: 180,
+      firstColor: '#e2f6fe',
+      secondColor: '#effdff',
+    },
+  },
+  blockquote:
+    'All day he hung round the cove or upon the cliffs with a brass telescope; all evening he sat in a corner of the parlour next the fire and drank rum and water very strong.',
+  author: 'Robert Louis Stevenson',
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  id: 'author',
+  type: 'checkbox',
+  label: 'Quote author',
+  defaultValue: true,
 }
 
 export default ExtendedWireframe
