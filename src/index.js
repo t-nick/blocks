3fe69c1763caffdfadae7a200f85889b5ceecba5
Sabina <sabina.mamedova90@gmail.com>
2018-02-09 23:48:03 +0200

    add some style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-series-1-quote/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -10,9 +10,23 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#f0f0f0',
+  },
+  blockquote:
+    'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.',
+  author: 'Helen Keller',
 }
 
-ExtendedWireframe.modifierScheme = []
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'author',
+    type: 'checkbox',
+    label: 'Quote author',
+    defaultValue: true,
+  },
+]
+
 
 export default ExtendedWireframe
