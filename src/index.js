a82ef1829b776f842d810c55a8fe3a1aca185098
Administrator <alexandr_bondarchuk@mail.ru>
2018-02-12 17:38:12 +0200

    Changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,82 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+  title: 'shortly<br>about us',
+  'text-1': 'Follow us:',
+  subtitle: 'We provide a wide range of services to meet even the most daring requirements. ',
+  text:
+    'Welcome to a number 1 source for hard to find and sold out event tickets online. Whether you are a sports fan looking to buy football tickets for the next home game or a fan searching for concert tickets, we have everything you need here in one place. There is no reason to miss your long-awaited game. You can buy any ticket you need from the comfort of your home.',
+  picture: {
+    resourceRef: 'image-01.png',
+    alt: 'Picture about the company',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'read more',
+    type: 'primary',
+    size: 'md',
+  },
+  'button-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'link',
+  },
+  link: '<a href="/">More about us</a>',
+  socialIcons: {
+    networks: [
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'youtube',
+        name: 'YouTube',
+        url: 'http://youtube.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 15,
+      color: '#9b9b9b',
+      sizes: [10, 20, 30, 40],
+      size: 30,
+    },
+  },
+  topIcon: {
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
+  background: {
+    type: 'color',
+    color: '#110c30',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
