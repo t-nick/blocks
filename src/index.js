d454425cd33578f0f7232146205ccfed58735c7b
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-02-15 10:20:36 -0800

    done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,27 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Teacher Training',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Become a teacher or just expand your practice ',
+    type: 'subtitlte',
+  },
+  text: {
+    content: 'Based on the tradition of authentic ancestors, our program is aimed at bringing yoga back to its true home as an alive art, and as a comprehensive way to revival. We are highly experienced teachers who are committed to providing a clear and encouraging education while enabling you to reveal your true abilities and potential. ',
+    type: 'text',
+  },
+  picture: {
+    src: 'photo.jpg',
+    alt: 'Picture about the company',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Subtitle', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
