1e6894c096a1e8ccf7f9c9d5999b7df9f5fe6ca1
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-02-15 09:30:00 -0800

    first push faq_services_deep

--- src/index.js
+++ src/index.js
@@ -11,6 +11,72 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  faq: [
+    {
+      title: {
+        content: 'Can I book a team for the weekend?',
+        type: 'heading',
+      },
+      description: {
+        content: 'Yes, this service is available 7 days per week, even on weekends and bank holidays. ',
+        type: 'text',
+      },
+    },
+    {
+      title: {
+        content: 'What is your coverage?',
+        type: 'heading',
+      },
+      description: {
+        content: 'Click Order button next to the service you want to purchase and leave your contacts in a follow-up form. We will get in touch with shortly to clarify all the details. ',
+        type: 'text',
+      },
+    },
+    {
+      title: {
+        content: 'Do you also clean kitchen appliances?',
+        type: 'heading',
+      },
+      description: {
+        content: 'Click Order button next to the service you want to purchase and leave your contacts in a follow-up form. We will get in touch with shortly to clarify all the details. ',
+        type: 'text',
+      },
+    },
+    {
+      title: {
+        content: 'When can I make a booking?',
+        type: 'heading',
+      },
+      description: {
+        content: 'Click Order button next to the service you want to purchase and leave your contacts in a follow-up form. We will get in touch with shortly to clarify all the details. ',
+        type: 'text',
+      },
+    },
+    {
+      title: {
+        content: 'Is there a difference between Residential and Deep Cleaning?',
+        type: 'heading',
+      },
+      description: {
+        content: 'Click Order button next to the service you want to purchase and leave your contacts in a follow-up form. We will get in touch with shortly to clarify all the details. ',
+        type: 'text',
+      },
+    },
+    {
+      title: {
+        content: 'Can I get video consultation with your specialist?',
+        type: 'heading',
+      },
+      description: {
+        content: 'How are the prices for Deep Cleaning formed?',
+        type: 'text',
+      },
+    },
+  ],
+  title: {
+    content: 'FAQ',
+    type: 'blockTitle',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
