f33200cc27d18ed3e2138c12867de161f27e9bdc
nataliadiadkova <nataliadiadkova@gmail.com>
2018-02-15 16:57:25 +0200

    design tauros cta_2_about_careers_support

--- src/index.js
+++ src/index.js
@@ -11,10 +11,37 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Thank you for applying for a job!',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'We will contact you shortly',
+    type: 'text',
+  },
+  theme: 'light',
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Back to Homepage',
+    type: 'secondary',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  picture: {defaultValue: false, label: 'Picture', type: 'hidden'},
 }
 
 export default ExtendedWireframe
