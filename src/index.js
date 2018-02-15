57ea3e7968e268fe028d94a55f9b1f33bc41f81a
JulBeet <julbeet@gmail.com>
2018-02-15 12:12:39 +0200

    init d/tauros/awards_1_homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-zapdos-awards/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,10 +11,74 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  awards: [
+    {
+      id: '8130928a-e7c6-4e26-a3ca-a820fa36380e',
+      title: 'R&D Leader in Tech (2017)',
+      picture: {
+        src: '1.png',
+        alt: 'Award illustration photo',
+      },
+    },
+    {
+      id: '057c27bf-4977-4b28-bf35-f4a6db60d5aa',
+      title: 'Most Innovative B2B Solutions (2016)',
+      picture: {
+        src: '2.png',
+        alt: 'Award illustration photo',
+      },
+    },
+    {
+      id: 'c8e93041-896b-4b78-b8d0-37e21a79070f',
+      title: 'Best Customer-Focused Approach on the World Conference (2014)',
+      picture: {
+        src: '3.png',
+        alt: 'Award illustration photo',
+      },
+    },
+    {
+      id: '3ea9fdda-c0e6-418e-9bfe-7bf1b06a534d',
+      title: 'Leader of Innovation (2012)',
+      picture: {
+        src: '4.png',
+        alt: 'Award illustration photo',
+      },
+    },
+    {
+      id: '3ea9fdda-c0e6-418e-9bfe-7bf1b06a534d1',
+      title: 'Leader of Innovation (2012)',
+      picture: {
+        src: '5.png',
+        alt: 'Award illustration photo',
+      },
+    },
+    {
+      id: '3ea9fdda-c0e6-418e-9bfe-7bf1b06a534d2',
+      title: 'Leader of Innovation (2012)',
+      picture: {
+        src: '6.png',
+        alt: 'Award illustration photo',
+      },
+    },
+  ],
+  title: 'Certificates',
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'StockSnap_M0YZ9Q79DZ.jpg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
+  heading: {defaultValue: false, label: 'Awards title', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Block description', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
