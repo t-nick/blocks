636dc4104898bd2537fdc09c782d5907a509c111
Sabina <sabina.mamedova90@gmail.com>
2018-02-15 15:19:26 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -10,11 +10,72 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+  theme: 'light',
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'cover-bg.jpg',
+  },
+  title: {
+    content: 'Quantum Company',
+    type: 'heroTitle',
+  },
+  subtitle: {
+    content: 'We can execute even the most unpredictable ideas! ',
+    type: 'subtitle',
+  },
+  'cta-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+    size: 'lg',
+  },
+  'cta-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+    size: 'lg',
+  },
+  topCaption: 'welcome',
 }
 
 ExtendedWireframe.modifierScheme = {
-  ...Component.modifierScheme,
+  align: {
+    children: [
+      {id: 'left', label: 'left'},
+      {id: 'center', label: 'center'},
+      {id: 'right', label: 'right'},
+    ],
+    defaultValue: 'left',
+    name: 'Aligning',
+    type: 'radio-button-group',
+  },
+  'caption-decorator': {defaultValue: false, label: 'Pretitle', type: 'hidden'},
+  'primary-btn': {defaultValue: true, label: 'Button', type: 'checkbox'},
+  'secondary-btn': {defaultValue: true, label: 'Additional button', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  undefined: {type: 'separator'},
 }
 
 export default ExtendedWireframe
