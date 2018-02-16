97a6e7f27860f57fcce8834bbd61972485599bf9
isergienko <igorserhiyenko@gmail.com>
2018-02-16 12:24:18 +0200

    title

--- src/index.js
+++ src/index.js
@@ -11,6 +11,52 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.png',
+  },
+  title: {
+    content: 'Receive the best useful tips on home maintenance!',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Sign up to get time-saving advice, cleaning secrets, and special offers.',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Name',
+        placeholder: 'Your Name',
+        required: true,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: '',
+        placeholder: '',
+        required: false,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Email',
+        placeholder: 'Your E-Mail',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Subscribe',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
