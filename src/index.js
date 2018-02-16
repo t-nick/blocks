d4771a16abd3538291fe97cf68bbe9a486d26787
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-02-16 11:58:57 +0200

    finish commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,44 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  theme: 'dark',
+  background: {
+    type: 'color',
+    color: 'dark-accent-color',
+  },
+  title: 'Get useful advice <strong>on how to keep your house shiny and clean longer!</strong>',
+  subtitle: 'We regularly share some helpful tips to make your cleaning routine faster and easier.',
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: '-',
+        placeholder: 'Your Name',
+        required: true,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: '',
+        placeholder: '',
+        required: false,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: '-',
+        placeholder: 'E-Mail',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Subscribe Now',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
