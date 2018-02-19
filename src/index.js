4f4a76cc8a64f2852ff54be86e2386da35af106e
alex_diachenko <diachenko_work@mail.ru>
2018-02-19 22:42:33 +0200

    first commit, ошибки описаны на доске

--- src/index.js
+++ src/index.js
@@ -11,10 +11,44 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#ffffff',
+  },
+  title: {
+    content: 'Want to work with us?',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'Sorry, but the page you are looking for could not be found',
+    type: 'text',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'back to homepage',
+    type: 'primary-alt',
+    size: 'lg',
+  },
+  picture: {
+    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+    alt: 'Illustration photo',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Title description', type: 'checkbox'},
+  picture: {defaultValue: false, label: 'Picture', type: 'hidden'},
 }
 
 export default ExtendedWireframe
