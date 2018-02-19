e57c33ea9ba77008bc1d78a52dde45953711d8be
Kirill Donenko <kirill.donenko@gmail.com>
2018-02-20 00:07:59 +0200

    styles of description

--- src/index.js
+++ src/index.js
@@ -11,10 +11,44 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#fff',
+  },
+  title: {
+    content: 'Thanks for your registration!',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'Roy will contact you shortly and provide all the additional info',
+    type: 'text',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Back to homepage',
+    type: 'primary',
+    size: 'lg',
+  },
+  picture: {
+    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+    alt: 'Illustration photo',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  picture: {defaultValue: false, label: 'Picture', type: 'hidden'},
 }
 
 export default ExtendedWireframe
