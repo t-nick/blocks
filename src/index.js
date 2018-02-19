998cc9d7ff0e1ae71f2a15f1611107798a07b249
alex_diachenko <diachenko_work@mail.ru>
2018-02-19 22:25:25 +0200

    first commit, ошибки описаны на доске

--- src/index.js
+++ src/index.js
@@ -11,10 +11,53 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.png',
+  },
+  title: {
+    content: 'Want to increase profits?',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Too many entrepreneurs are afraid of sales calls. Learn how to make the sale, always.',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Name',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Email',
+        placeholder: '',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Subscribe',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
