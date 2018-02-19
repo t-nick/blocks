a978ea4fcfdd0606a3b031c996960a026566cce3
Vladimir Zelik <kazantip81v@gmail.com>
2018-02-20 01:03:12 +0200

    add content & styles

--- src/index.js
+++ src/index.js
@@ -11,10 +11,47 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'layer-240.jpg',
+    overlay: {
+      type: 'color',
+      color: 'var(--ui-dark-shade-color)',
+      opacity: 0.25,
+    },
+  },
+  title: {
+    content: 'Want to Get a Consultation that Can <br/>Help Your Business Grow?',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'Contact Roy and describe your issue',
+    type: 'text',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Subscribe',
+    type: 'primary-alt',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
