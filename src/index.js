84891b4d98957c40c3171d8196bf06b5e3e8cf96
VladReznik <vlad_escape@ukr.net>
2018-02-20 15:33:29 +0200

    First Push

--- src/index.js
+++ src/index.js
@@ -11,10 +11,23 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Coaching',
+    type: 'blockTitle',
+  },
+  text: {
+    content: 'If you are ready to new perspectives, if you are dedicated to your self-growth, and you are ready for things to be changed, here is the best way to reach your aspired results.',
+    type: 'text',
+  },
+  picture: {
+    src: 'bg.png',
+    alt: 'Picture about the company',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  button: false,
 }
 
 export default ExtendedWireframe
