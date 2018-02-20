37d0469ddbce15f6d636ab16e0795fe0202a7fd2
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-02-20 11:22:38 +0200

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,6 +11,46 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  title: {
+    content: 'Sign up for our newsletter',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Receive our weekly business coaching useful tips and latest news right into your inbox.',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Name',
+        placeholder: 'Name',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Email',
+        placeholder: 'Email',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Subscribe',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
