7f79844748111b68c3e591e6b778d221ab288b68
Sabina <sabina.mamedova90@gmail.com>
2018-02-20 21:35:02 +0200

    start

--- src/index.js
+++ src/index.js
@@ -10,11 +10,55 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+  theme: 'light',
+  background: {
+    type: 'color',
+    color: 'var(--ui-light-shade-color)',
+  },
+  title: {
+    content: 'Want to increase profits?',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Complete the form below and receive a full analysis of your business within 2 days!',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Name',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: 'Phone number ',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Email',
+        placeholder: '',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Request a quote',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
-  ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
