f23d2f1cc622393e9d85de21f459689a9a66bda4
Sabina <sabina.mamedova90@gmail.com>
2018-02-20 22:10:10 +0200

    add some stiles and content

--- src/index.js
+++ src/index.js
@@ -10,11 +10,143 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Service 1',
+        type: 'heading',
+      },
+      description: {
+        content: 'Episode 9: How to overcome a fear of starting a business with Jeff Tyler',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'pic1.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Go to episode',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Service 2',
+        type: 'heading',
+      },
+      description: {
+        content: 'Episode 8: How the community is important to build your private practice with Jason Green',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'pic2.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Go to episode',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Service 3',
+        type: 'heading',
+      },
+      description: {
+        content: 'Episode 7: How to create a six-figure business with Thomas Perry',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'pic3.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Go to episode',
+        type: 'link',
+      },
+    },
+  ],
+  topIcon: {
+    svg:
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
+  title: {
+    content: 'Services We Provide',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'We deliver all kind of services that support small and micro businesses. Here are some of them:',
+    type: 'subtitle',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+  },
+  'button-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'all episodes',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
-  ...Component.modifierScheme,
+  body: {defaultValue: true, label: 'Service main text', type: 'checkbox'},
+  'button-primary': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
+  'button-secondary': {defaultValue: true, label: 'Button', type: 'checkbox'},
+  heading: {defaultValue: true, label: 'Service title', type: 'checkbox'},
+  link: {defaultValue: true, label: 'Link', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Services description', type: 'checkbox'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
