dee0c91b2846ca8f631b9b55778aec9b538f1c12
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-02-20 09:37:27 -0800

    done hypno/cta-message/dev

--- src/index.js
+++ src/index.js
@@ -11,10 +11,40 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Thanks for your message!',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'Roy will get in touch with you shortly',
+    type: 'text',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'back to homepage',
+    type: 'primary-alt',
+    size: 'lg',
+  },
+  theme: 'light',
+
+  background: {
+    type: 'color',
+    color: 'ui-light-shade-color',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
