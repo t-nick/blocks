fa4206bf0dc83790991db2a59d093f482d2edf17
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-02-20 10:23:38 -0800

    done d/hypno/services_2/dev

--- src/index.js
+++ src/index.js
@@ -11,10 +11,115 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Coaching',
+    type: 'blockTitle',
+  },
+  'button-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'all coaching services',
+    type: 'secondary',
+  },
+  services: [
+    {
+      title: {
+        content: 'Executive',
+        type: 'heading',
+      },
+      description: {
+        content: 'Multipurpose assistance for online stores and offline retail businesses.',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_1.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Corporate',
+        type: 'heading',
+      },
+      description: {
+        content: 'Developing marketing and business strategy to help company grow fast.',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_2.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Small Busniess',
+        type: 'heading',
+      },
+      description: {
+        content: 'Building brand awareness for micro and small businesses. ',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_3.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  body: {defaultValue: false, label: 'Service main text', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
