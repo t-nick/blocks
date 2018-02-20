bd9e7e2a138b3b255561da08c725937971203ff8
VladReznik <vlad_escape@ukr.net>
2018-02-20 15:25:29 +0200

    First Push

--- src/index.js
+++ src/index.js
@@ -11,10 +11,23 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Speaking',
+    type: 'blockTitle',
+  },
+  text: {
+    content: 'Roy Hale is a progressive business keynote speaker who fascinates audiences with interactive, no fluff, high-value content. Born in San Francisco to become an accidental Millionaire by the age 29, Roy motivates ordinary entrepreneurs to make an exceptional impact.',
+    type: 'text',
+  },
+  picture: {
+    src: 'bg.png',
+    alt: 'Picture about the company',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  button: false,
 }
 
 export default ExtendedWireframe
