651b881302033b1c285952811ca90d1f77343fcb
VladReznik <vlad_escape@ukr.net>
2018-02-20 15:38:04 +0200

    First Push

--- src/index.js
+++ src/index.js
@@ -11,10 +11,23 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Workshops',
+    type: 'blockTitle',
+  },
+  text: {
+    content: 'Roy hosts regular workshops on subjects such as business planning and strategy, goal setting & action plans, sales for small businesses, and much more. Check out our workshop calendar or get in touch to register for the next workshop presentation.',
+    type: 'text',
+  },
+  picture: {
+    src: 'bg.png',
+    alt: 'Picture about the company',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  button: false,
 }
 
 export default ExtendedWireframe
