2ec7e19fe44e7981dd913880c3b7c76a325a8793
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-22 14:55:50 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -25,10 +25,6 @@ ExtendedWireframe.defaultContent = {
       opacity: 0.92,
     },
   },
-  subtitle: {
-    type: 'subtitle',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  },
   partners: [
     {
       picture: {
