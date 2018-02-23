40cc203fcd62b00ef1907186b927c55b4fd8d98c
max-sindi <sindm93@mail.ru>
2018-02-23 12:50:22 +0200

    done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,38 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#f8f8f8',
+  },
+  title: {
+    content: 'About me',
+    type: 'blockTitle',
+  },
+  text: {
+    content:
+     'Hi, I\'m Roy Hale, a head coach at Roy Hale Business Coaching. If you are ready for changes, let’s build a profitable business. \n\nBuilding and running any business is not an easy task. It\'s rather expensive and it takes all your time and efforts. Hiring a business coach can help you create a more successful organization. \n\nLet me show you how I can develop a great strategy and help you achieve your dream of a profitable company.',
+    type: 'text',
+  },
+  picture: {
+    resourceRef: 'picture.png',
+    alt: 'Roy Hale photo',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Know more about me',
+    type: 'secondary-alt',
+    size: 'md',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
