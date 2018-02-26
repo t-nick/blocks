74da56b41b860f3d86e76b002fde9bd9d90b2d06
zhtbcgtw <ivanov.i@weblium.com>
2018-02-26 17:26:31 +0200

    update defaultContent dark theme background

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ ExtendedWireframe.defaultContent = {
         type: 'heading',
       },
       description: {
-        content: 'Roy will share the essential steps of business planning and the main key elements for business growth. His simple approach to strategic planning steps will help you determine and meet your business goals, operational milestones, and sales objectives.',
+        content:
+          'Roy will share the essential steps of business planning and the main key elements for business growth. His simple approach to strategic planning steps will help you determine and meet your business goals, operational milestones, and sales objectives.',
         type: 'text',
       },
       category: {
@@ -54,7 +55,8 @@ ExtendedWireframe.defaultContent = {
         type: 'heading',
       },
       description: {
-        content: 'Roy will run you through his six key steps to putting a dynamic but simple business plan. Whether you’re a verified small business eager to take your business to the next level, or a start-up at the beginning of your journey, this workshop is right for you.',
+        content:
+          'Roy will run you through his six key steps to putting a dynamic but simple business plan. Whether you’re a verified small business eager to take your business to the next level, or a start-up at the beginning of your journey, this workshop is right for you.',
         type: 'text',
       },
       category: {
@@ -86,7 +88,8 @@ ExtendedWireframe.defaultContent = {
     },
   ],
   topIcon: {
-    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    svg:
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
     fill: 'red',
   },
   title: {
