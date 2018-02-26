d9a23753c229f3051bb5503e6f63c487ffd408fe
zhtbcgtw <ivanov.i@weblium.com>
2018-02-26 18:04:54 +0200

    update defaultContent dark theme background

--- src/index.js
+++ src/index.js
@@ -12,16 +12,17 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   theme: 'dark',
+  background: {type: 'color', color: 'dark-shade-color'},
   subtitle: {
-    content: 'When it comes to something as creative as tattoos or piercings, the price tag applies based on every specific size, difficulty, and colors of any given body artwork piece.',
+    content:
+      'When it comes to something as creative as tattoos or piercings, the price tag applies based on every specific size, difficulty, and colors of any given body artwork piece.',
     type: 'subtitle',
   },
   services: [
     {
       id: '900aa334-14f2-4c05-b078-78099a5725e5',
       title: '$100 – $150',
-      description:
-        'Multipurpose assistance for online stores and offline retail businesses.',
+      description: 'Multipurpose assistance for online stores and offline retail businesses.',
       picture: {
         resourceRef: 'services1.jpg',
         alt: 'Service illustration photo',
@@ -44,8 +45,7 @@ ExtendedWireframe.defaultContent = {
     {
       id: 'cfb2c56d-7708-4459-bdf2-6780f4ba62b9',
       title: '$150 – $500',
-      description:
-        'Developing marketing and business strategy to help company grow fast.',
+      description: 'Developing marketing and business strategy to help company grow fast.',
       picture: {
         resourceRef: 'services2.jpg',
         alt: 'Service illustration photo',
@@ -68,8 +68,7 @@ ExtendedWireframe.defaultContent = {
     {
       id: 'eea21546-b56e-406b-8cba-18a5a5ec1820',
       title: '$500 – $1000',
-      description:
-        'Building brand awareness for micro and small businesses. ',
+      description: 'Building brand awareness for micro and small businesses. ',
       picture: {
         resourceRef: 'services3.jpg',
         alt: 'Service illustration photo',
@@ -92,8 +91,7 @@ ExtendedWireframe.defaultContent = {
     {
       id: 'aa5d3a04-43b7-4bb3-9c7a-00e1986f359e',
       title: '$1000+',
-      description:
-        'Aftersales support in marketing, sales, and staff training.',
+      description: 'Aftersales support in marketing, sales, and staff training.',
       picture: {
         resourceRef: 'services4.jpg',
         alt: 'Service illustration photo',
