0be0b63e98d3aaf8baa521c51dbc317bb8b0b98d
nmelnychenko <mykola.melnichenko@gmail.com>
2018-02-26 15:04:51 +0200

    make work counter wireframe

--- src/component.js
+++ src/component.js
@@ -19,14 +19,15 @@ class Block extends React.Component {
       <section className={css.section}>
         <div className={css.section__inner}>
           <Text tagName="h1" className={css.title} bind="title" />
-          <Counter bind="counter">
-            {date => ['days', 'hours', 'seconds', 'minutes'].map((type, index) => (
-              <div className={css.counter__item}>
-                <Text tagName="strong" value={{content: date[type], type: 'blockTitle'}} />
-                {/* <strong className={css.counter__number}>{date[type]}</strong> */}
-                <Text tagName="p" className={css.counter__text} bind={`items[${index}].text`} />
-              </div>))}
-          </Counter>
+          <div className={css.counter}>
+            <Counter bind="counter">
+              {date => ['days', 'hours', 'minutes', 'seconds'].map((type, index) => (
+                <div key={type} className={css.counter__item}>
+                  <Text tagName="strong" className={css.counter__number} value={{content: `${date[type]}`, type: 'blockTitle'}} />
+                  <Text tagName="p" className={css.counter__text} bind={`items[${index}].text`} />
+                </div>))}
+            </Counter>
+          </div>
           <div className="content">
             <div className="content__inner">
               <Text tagName="h2" className="content__title" bind="heading" />
@@ -46,21 +47,21 @@ class Block extends React.Component {
 
 Block.components = _.pick(['Text', 'Counter', 'Button'])($editor.components)
 
-
 Block.defaultContent = {
-  title: 'About The Company',
-  'text-1': 'Follow us:',
-  subtitle:
-    'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
-  text:
-    'We are in it for the long haul—for our customers and for our world. Our customers can be found in virtually every corner of the earth, and we realize our success comes directly from helping our customers be successful. We take seriously our responsibility to give back to the communities in which we work and live.',
-  picture: {
-    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-    alt: 'Picture about the company',
+  title: {
+    type: 'blockTitle',
+    content: 'SPECIAL OFFER',
+  },
+  heading: {
+    type: 'largeHeading',
+    content: 'Get Pro Plan at the Price of Standard',
+  },
+  subtitle: {
+    type: 'subtitle',
+    content: 'Deal expires December, 13',
   },
   counter: {
     futureDate: new Date(2018, 1, 28),
-    type: 'blockTitle',
   },
   items: [
     {
@@ -88,7 +89,7 @@ Block.defaultContent = {
       },
     },
   ],
-  button: {
+  cta: {
     actionConfig: {
       action: 'link',
       actions: {
@@ -99,49 +100,9 @@ Block.defaultContent = {
         },
       },
     },
-    textValue: 'Contact us',
-  },
-  link: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'More about us',
-  },
-  socialIcons: {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'youtube',
-        name: 'YouTube',
-        url: 'http://youtube.com/',
-      },
-    ],
-    target: '_blank',
-    design: {
-      border: 'circle',
-      innerFill: true,
-      preset: 'preset001',
-      padding: 20,
-      color: '',
-      sizes: [10, 20, 30, 40],
-      size: '40px',
-    },
+    type: 'primary',
+    size: 'lg',
+    textValue: 'Request a quote',
   },
 }
 
