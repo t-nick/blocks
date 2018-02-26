c17ba67337dfa89812a5f8966aa04938bce37c47
zhtbcgtw <ivanov.i@weblium.com>
2018-02-26 17:18:18 +0200

    update defaultContent dark theme background

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 ExtendedWireframe.defaultContent = {
   title: 'Welcome to Our Company ',
   'text-1': 'Follow us:',
-  subtitle: 'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
+  subtitle:
+    'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
   text:
     'We want to share that passion with our customers who come far and wide to enjoy the food we have spent sleepless nights thinking up, testing and producing at the very highest standard possible.',
   picture: {
@@ -85,7 +86,8 @@ ExtendedWireframe.defaultContent = {
     },
   },
   topIcon: {
-    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    svg:
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
     fill: 'red',
   },
 }
