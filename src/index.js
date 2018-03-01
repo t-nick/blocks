7eeea6f63ccf659e0a582fb03671f3b284715afd
VladReznik <vlad_escape@ukr.net>
2018-03-01 18:41:44 +0200

    First

--- src/index.js
+++ src/index.js
@@ -11,10 +11,47 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  theme: 'dark',
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'background.png',
+  },
+  title: {
+    content: 'Feel yourself more alive than ever',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'While enjoying the flow of adrenaline during a freefall at speed over 100mph, you will also have the opportunity to see the city and the neighborhoods from the bird\'s eye view. Drift in the sky savoring the majestic beauty of the picturesque surroundings.',
+    type: 'text',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary-alt',
+    size: 'lg',
+  },
+  picture: {
+    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+    alt: 'Illustration photo',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
