5f09b79d98204fff6be3906d79bcc151b5ae4dfe
nataliadiadkova <nataliadiadkova@gmail.com>
2018-03-01 10:19:05 +0200

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,38 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#ffffff',
+  },
+  title: {
+    content: 'Thank you for the message!',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'We will contact you shortly',
+    type: 'subheading',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'back to home page',
+    type: 'primary',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
