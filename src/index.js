94da3e5fec1f92fb3f928c25c1869f698d864559
JulBeet <julbeet@gmail.com>
2018-03-01 21:57:49 +0200

    gesign markup added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-zapdos-numbers/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,10 +11,95 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'some facts in Numbers',
+    type: 'blockTitle',
+  },
+  numbers: [
+    {
+      title: {
+        content: 'Increase profits by',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Years of experience and practice',
+        type: 'text',
+      },
+      value: {
+        content: '21',
+        type: 'blockTitle',
+      },
+    },
+    {
+      title: {
+        content: 'Expand market reach for',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Airplanes that can accommodate 112 passengers',
+        type: 'text',
+      },
+      value: {
+        content: '6',
+        type: 'blockTitle',
+      },
+    },
+    {
+      title: {
+        content: 'Improve customer satisfaction for',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Trustworthy instructors',
+        type: 'text',
+      },
+      value: {
+        content: '50',
+        type: 'blockTitle',
+      },
+    },
+    {
+      title: {
+        content: 'Improve customer satisfaction for',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Positive reviews from happy clients',
+        type: 'text',
+      },
+      value: {
+        content: '13,650',
+        type: 'blockTitle',
+      },
+    },
+    {
+      title: {
+        content: 'Improve customer satisfaction for',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Skydiving jumps each year',
+        type: 'text',
+      },
+      value: {
+        content: '175,000',
+        type: 'blockTitle',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  body: {defaultValue: true, label: 'Numbers description', type: 'checkbox'},
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
+  'number-title': {
+    defaultValue: false,
+    label: 'Additional information on numbers',
+    type: 'checkbox',
+  },
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
