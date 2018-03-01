39706b87bdb2017c72407711dcaf7701b2350d7b
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-03-01 06:49:07 -0800

    about_3

--- src/index.js
+++ src/index.js
@@ -11,10 +11,20 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  picture: {
+    src: 'image.jpg',
+    alt: 'Picture about the company',
+  },
+  text: {
+    content: 'SkydiveBirds was founded in 1997. For over 20 years we provide our members with everything they need to learn to skydive in a safe and friendly environment and try to satisfy any skydiving needs and requirements. Our professional pilots and highly qualified instructors will provide you with the individual attention and will make your experience a life-long memory. So if you want to spend a great time, don\'t hesitate to spend it with us!',
+    type: 'text',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
+  title: {defaultValue: false, label: 'Block title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
