6c0a32a652de5c0c61f1bea881c332d15cf78942
tangerine69 <mrtangerine69@gmail.com>
2018-03-01 13:28:49 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-zapdos-cta/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,10 +11,46 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  title: {
+    content: 'Feel yourself more alive than ever!',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'While enjoying the flow of adrenaline during a freefall at speed over 100mph, you will also have the opportunity to see the city and the neighborhoods from the bird\'s eye view. Drift in the sky savoring the majestic beauty of the picturesque surroundings.',
+    type: 'text',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'BOOK A SKYDIVE NOW',
+    type: 'primary',
+    size: 'lg',
+  },
+  picture: {
+    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+    alt: 'Illustration photo',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
