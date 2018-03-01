048851b9aabb71c158adbbfda6850718b81904de
JulBeet <julbeet@gmail.com>
2018-03-01 18:59:43 +0200

    design markup added

--- src/index.js
+++ src/index.js
@@ -11,10 +11,52 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'light-shade-color',
+  },
+  title: {
+    content: 'for more information request a quote and we will contact you to specify the details',
+    type: 'blockTitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: '',
+        placeholder: 'Your name',
+        required: false,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: '',
+        placeholder: 'Email',
+        required: false,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: '',
+        placeholder: 'Phone number ',
+        required: false,
+      },
+    ],
+    submitButton: {
+      title: 'Request a quote',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: false, label: 'Title description', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+
 }
 
 export default ExtendedWireframe
