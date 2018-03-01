a070baad7ca207cef0a96adbc407f40f2b25ec80
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-03-01 17:56:46 +0200

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,6 +11,50 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  formBackground: {
+    type: 'color',
+    color: 'transparent',
+  },
+  title: {
+    content: 'Get your pdf training manual to prepare for aff program',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Ready to go skydiving? Receive helpful tips for your first tandem skydive',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Name',
+        placeholder: 'Name',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Email',
+        placeholder: 'Email',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Get manual',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
