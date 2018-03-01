a23b94acbbe317a7717c187871c89279472c0177
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-03-01 18:45:33 +0200

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  title: {
+    content: 'Make a difference!<br>Partake in charitable skydiving!',
+    type: 'blockTitle',
+  },
+  text: {
+    content: 'Become a part of the solution to the world’s challenges by participating and skydiving for charity at $50. Just select a charity event from the calendar, pay your $50 deposit and raise the minimum fundraising amount. We work with 100\'s of charities across the USA. Over 2000 people every year choose to tandem skydive in aid of charity with SkydiveBirds.',
+    type: 'text',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
