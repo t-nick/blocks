fb49a9a781b2a33d5296c3441f3bdfd8b79aad97
isergienko <igorserhiyenko@gmail.com>
2018-03-01 19:44:04 +0200

    done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,69 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  team: [
+    {
+      name: {
+        content: 'Mary Winslow',
+        type: 'heading',
+      },
+      position: {
+        content: '13,000 jumps',
+        type: 'caption',
+      },
+      picture: {
+        resourceRef: 'marry.png',
+        alt: 'Andrew Shimmer photo',
+      },
+    },
+    {
+      name: {
+        content: 'Stuart Bean',
+        type: 'heading',
+      },
+      position: {
+        content: '16,000 jumps',
+        type: 'caption',
+      },
+      picture: {
+        resourceRef: 'stuart.png',
+        alt: 'Ann Maisner photo',
+      },
+    },
+    {
+      name: {
+        content: 'Derrick Jackson',
+        type: 'heading',
+      },
+      position: {
+        content: '18,000 jumps',
+        type: 'caption',
+      },
+      picture: {
+        resourceRef: 'derrick.png',
+        alt: 'Tomas Abbar photo',
+      },
+    },
+  ],
+  title: {
+    content: 'Owners',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Our team is our proud. Each of the team members is dedicated to delivering the best results to the clients. Meet some of our expert who will work on your case:',
+    type: 'subtitle',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'block-button': {defaultValue: false, label: 'Button', type: 'checkbox'},
+  'member-desc': {defaultValue: false, label: 'Team member bio', type: 'checkbox'},
+  'member-email': {defaultValue: false, label: 'Team member email', type: 'checkbox'},
+  'member-link': {defaultValue: false, label: 'Team member link', type: 'checkbox'},
+  'member-social': {defaultValue: false, label: 'Team social buttons', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Team description', type: 'checkbox'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
