218e642ad4ad3915d4069069d8871c72392a7f15
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-03-01 07:22:05 -0800

    done about_3_about/dev

--- src/index.js
+++ src/index.js
@@ -19,12 +19,16 @@ ExtendedWireframe.defaultContent = {
     content: 'SkydiveBirds was founded in 1997. For over 20 years we provide our members with everything they need to learn to skydive in a safe and friendly environment and try to satisfy any skydiving needs and requirements. Our professional pilots and highly qualified instructors will provide you with the individual attention and will make your experience a life-long memory. So if you want to spend a great time, don\'t hesitate to spend it with us!',
     type: 'text',
   },
+  title: {
+    content: 'About US',
+    type: 'blockTitle',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
   button: {defaultValue: false, label: 'Button', type: 'checkbox'},
-  title: {defaultValue: false, label: 'Block title', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
