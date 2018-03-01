1586eb87cf6413c5847e08280932f3f02d2246f6
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-03-01 04:59:08 -0800

    done natu/gallery_about/dev

--- src/index.js
+++ src/index.js
@@ -11,6 +11,98 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  gallery: [
+    {
+      title: {
+        content: 'Quantum CEO & our partners',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_1.jpg',
+        alt: 'Quantum CEO & our partners',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+    {
+      title: {
+        content: 'Digital Marketing workshop lecturer',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_2.jpg',
+        alt: 'Digital Marketing workshop lecturer',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+    {
+      title: {
+        content: 'Quantum Company at WebSummit',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_3.jpg',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+    {
+      title: {
+        content: 'UX workshop attendees',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_4.jpg',
+        alt: 'UX workshop attendees',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+    {
+      title: {
+        content: 'UX workshop lecturer',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_5.jpg',
+        alt: 'UX workshop lecturer',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+    {
+      title: {
+        content: 'Quantum team',
+        type: 'text',
+      },
+      picture: {
+        src: 'image_6.jpg',
+        alt: 'Quantum team',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
