72d1f1dbbec63c8f3b08ccb13e6fac800d76efca
Sabina <sabina.mamedova90@gmail.com>
2018-03-02 17:08:23 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -10,11 +10,74 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg-image.jpg',
+  },
+
+  title: {
+    content: 'Enjoy<br>the Jump<br>of a Lifetime!',
+    type: 'heroTitle',
+  },
+  subtitle: {
+    content: 'Providing the safest and the most fun & thrilling experience',
+    type: 'subtitle',
+  },
+  'cta-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'BOOK A SKYDIVE NOW',
+    type: 'primary',
+    size: 'lg',
+  },
+  'cta-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+    size: 'lg',
+  },
+  topCaption: 'welcome',
 }
 
 ExtendedWireframe.modifierScheme = {
-  ...Component.modifierScheme,
+  align: {
+    children: [
+      {id: 'left', label: 'left'},
+      {id: 'center', label: 'center'},
+      {id: 'right', label: 'right'},
+    ],
+    defaultValue: 'left',
+    name: 'Aligning',
+    type: 'radio-button-group',
+    style: 'buttons',
+  },
+  'caption-decorator': {defaultValue: false, label: 'Pretitle', type: 'hidden'},
+  'primary-btn': {defaultValue: true, label: 'Button', type: 'checkbox'},
+  'secondary-btn': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  undefined: {type: 'separator'},
 }
+
 
 export default ExtendedWireframe
