3c1dff0061e01a6a6c2c8a0c2dd50c2773a82d55
VladReznik <vlad_escape@ukr.net>
2018-03-02 17:26:21 +0200

    First push

--- src/index.js
+++ src/index.js
@@ -11,10 +11,34 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'background.png',
+  },
+  title: {
+    content: 'Experienced skydivers',
+    type: 'blockTitle',
+  },
+  heading: {
+    content: 'Come fly with us!',
+    type: 'heading',
+  },
+  subheading: {
+    content: 'At SkydiveBirds club, we appreciate our dedicated fun jumper community. We welcome experienced skydivers of all skill levels and ages!Our dropzone is located near Chicago. We are proud of our spacious facility and are passionate about what we do here. Our team of professional skydiving instructors and support staff are specially trained to work with the first-time tandem skydivers so you have nothing to worry about.',
+    type: 'subheading',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  heading: {defaultValue: true, label: 'Title', type: 'checkbox'},
+  subheading: {defaultValue: true, label: 'Subtitle', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  text: {defaultValue: false, label: 'Main text', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
