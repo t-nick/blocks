2c84be80266e3c73e70ea081b2a79fb5cf83d444
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-03-02 05:33:18 -0800

    done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,42 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'image1.jpg',
+  },
+  title: {
+    content: 'Enjoy the view on your way down!',
+    type: 'blockTitle',
+  },
+  description: {
+    content: 'While enjoying the flow of adrenaline during a freefall at speed over 100mph, you will also have the opportunity to see the city and the neighborhoods from the bird\'s eye view. Drift in the sky savoring the majestic beauty of the picturesque surroundings.',
+    type: 'text',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'book a skydive now',
+    type: 'primary-alt',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
