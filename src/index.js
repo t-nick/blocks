88e8b8d18feac71ce70f1e5977045fd05ad0ad22
Kirill Donenko <kirill.donenko@gmail.com>
2018-03-02 19:19:57 +0200

    styles of block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,59 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#d8d8d8',
+  },
+  formBackground: {
+    type: 'color',
+    color: 'brand-color',
+  },
+  title: {
+    content: 'Get your PDF instructions for a first jump!',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Ready to go skydiving? Receive helpful tips for your first tandem skydive.',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Name',
+        placeholder: 'Name',
+        required: true,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: 'Phone number ',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Email',
+        placeholder: 'Email',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Get instructions',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-lg button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
