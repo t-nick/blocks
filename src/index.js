de07d70962215f089653d02725c6514f5630b19b
isergienko <igorserhiyenko@gmail.com>
2018-03-02 15:47:44 +0200

    about_text

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Film Production at SkydiveBirds',
+    type: 'blockTitle',
+  },
+  text: {
+    content: 'Our spacious 15,000 sq ft facility is located on the five-acre campus on the Rochelle Municipal Airport, Chicago. Our private airport is proud to offer film production services to help you capture all the beauty of the spectacular views of the surrounding area. We have state of the art aircraft and expert instructors to ensure your safety.',
+    type: 'text',
+  },
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.png',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
