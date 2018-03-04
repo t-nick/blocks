03e993c0edc331a3fa028a6378b88f3d7be51668
max-sindi <britva.okkamaa@gmail.com>
2018-03-04 15:43:08 +0200

    design done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,76 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Stunt Coordination and Cinematography',
+    type: 'blockTitle',
+  },
+  services: [
+    {
+      title: {
+        content: 'Service 1',
+        type: 'heading',
+      },
+      description: {
+        content:
+          'We have everything you need for your new film or any other project. We offer a picturesque location, state of the art aircraft, professional skydivers, all the needed skydiving equipment, and aerial cinematography.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'service-1.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Service 2',
+        type: 'heading',
+      },
+      description: {
+        content: 'On filming projects, we work back-to-back with Joe Jennings, a \ntwo-time winner of the Emmy Award for cinematography and XGames champion. Joe’s work includes Charlie’s Angels, Air Force One, Triple XXX, San Andreas, Point Break, and more as well as many thousands of television and commercial projects. Joe was drawn to SkydiveBirds for its unparalleled landscape in Chicago, and he’s been filming here ever since.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'service-2.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  heading: {defaultValue: false, label: 'Service title', type: 'checkbox'},
+  link: {defaultValue: false, label: 'Link', type: 'checkbox'},
+  'button-secondary': {defaultValue: false, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
