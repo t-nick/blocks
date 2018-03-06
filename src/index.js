76133c4e4baaa383ccba483993ad13e70bf02954
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-03-06 16:52:29 +0200

    finish style design block without star rating

--- src/index.js
+++ src/index.js
@@ -11,10 +11,86 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  testimonials: [
+    {
+      title: {
+        content: 'Julia Banks',
+        type: 'heading',
+      },
+      date: {
+        content: 'October 28, 2017',
+        type: 'caption',
+      },
+      description: {
+        content: '“They are always accountable. I have confidence when I make a decision. I know it\'s going to get done. If they could lose money on a project just because they\'re trying to make the customer happy, they probably would do that.”',
+        type: 'text',
+      },
+      position: {
+        content: 'CEO at minor thinking',
+        type: 'caption',
+      },
+      picture: {
+        alt: 'Amanda Peterson review',
+        src: 'review.png',
+      },
+    },
+    {
+      title: {
+        content: 'Julia Banks',
+        type: 'heading',
+      },
+      date: {
+        content: 'October 28, 2017',
+        type: 'caption',
+      },
+      description: {
+        content: '“They are always accountable. I have confidence when I make a decision. I know it\'s going to get done. If they could lose money on a project just because they\'re trying to make the customer happy, they probably would do that.”',
+        type: 'text',
+      },
+      position: {
+        content: 'CEO at minor thinking',
+        type: 'caption',
+      },
+      picture: {
+        alt: 'Jeff Gordons review',
+      },
+    },
+    {
+      title: {
+        content: 'Julia Banks',
+        type: 'heading',
+      },
+      date: {
+        content: 'October 28, 2017',
+        type: 'caption',
+      },
+      description: {
+        content: '“They are always accountable. I have confidence when I make a decision. I know it\'s going to get done. If they could lose money on a project just because they\'re trying to make the customer happy, they probably would do that.”',
+        type: 'text',
+      },
+      position: {
+        content: 'CEO at minor thinking',
+        type: 'caption',
+      },
+      picture: {
+        alt: 'Elizabeth Smith review',
+      },
+    },
+  ],
+  subtitle: {
+    ...Component.defaultContent.subtitle,
+    content: 'The French Revolution constituted for the conscience of the dominant aristocratic class a fall from innocence, and upturning of the natural chain of',
+    color: 'var(--ui-dark-shade-color)',
+  },
+  button: {
+    ...Component.defaultContent.button,
+    textValue: 'Additional button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Testimonials description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
