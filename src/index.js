60a976e153b6e112a85144c7254efc7435da0e32
tangerine69 <mrtangerine69@gmail.com>
2018-03-06 15:06:00 +0200

    fixed numbers

--- src/index.js
+++ src/index.js
@@ -13,31 +13,50 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   steps: [
     {
-      id: '07a5468d-b105-4866-98a5-dcf2cf46db3a',
-      title: '1.',
-      description:
-        'The cleaner will first attach the extension pole to the water pump.',
+      title: {
+        content: 'Pick a plan',
+        type: 'heading',
+      },
+      description: {
+        content: 'The cleaner will first attach the extension pole to the water pump.',
+        type: 'text',
+      },
     },
     {
-      id: '12e39a62-f8f4-4bf5-b2a0-5d536eeecd27',
-      title: '2.',
-      description:
-        'Then the fluid will be delivered under the high pressure from a specific brush head attachment, which will be moved across the whole window surface to treat all the stains.',
+      title: {
+        content: 'Make a payment',
+        type: 'heading',
+      },
+      description: {
+        content: 'Then the fluid will be delivered under the high pressure from a specific brush head attachment, which will be moved across the whole window surface to treat all the stains.',
+        type: 'text',
+      },
     },
     {
-      id: '5095f30d-1fbc-4809-a8c5-82bb203f3505',
-      title: '3.',
-      description:
-        'After duplicating this process twice and using the brush itself to scrub off any remaining stains, the professional will let the water evaporate on its own. This will keep your window panes dirt- and dust-free for a longer time than any common cleaning methods that are used currently.',
+      title: {
+        content: 'Make a payment',
+        type: 'heading',
+      },
+      description: {
+        content: 'After duplicating this process twice and using the brush itself to scrub off any remaining stains, the professional will let the water evaporate on its own. This will keep your window panes dirt- and dust-free for a longer time than any common cleaning methods that are used currently.',
+        type: 'text',
+      },
     },
     {
-      id: '3195f48d-96bc-a9d9-a9c5-71bb203f8405',
-      title: '4.',
-      description:
-        'On your request, your windows may be washed from the inside by our specialist. He will use purified water and squeegees for this and will stay until the moment that all stains are completely removed.',
+      title: {
+        content: 'Enjoy our products',
+        type: 'heading',
+      },
+      description: {
+        content: 'On your request, your windows may be washed from the inside by our specialist. He will use purified water and squeegees for this and will stay until the moment that all stains are completely removed.',
+        type: 'text',
+      },
     },
   ],
-  title: 'How it works',
+  title: {
+    content: 'How it works',
+    type: 'blockTitle',
+  },
   background: {
     type: 'color',
     color: '#fff',
@@ -48,7 +67,7 @@ ExtendedWireframe.modifierScheme = {
   body: {defaultValue: true, label: 'Step main text', type: 'checkbox'},
   button: {defaultValue: false, label: 'Button', type: 'checkbox'},
   'additional-button': {defaultValue: false, label: 'Button', type: 'hidden'},
-  heading: {defaultValue: true, label: 'Step title', type: 'checkbox'},
+  heading: {defaultValue: false, label: 'Step title', type: 'checkbox'},
   icon: {defaultValue: true, label: 'Step icon', type: 'checkbox'},
   subtitle: {defaultValue: false, label: 'Process description', type: 'checkbox'},
   title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
