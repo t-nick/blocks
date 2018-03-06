12da1015f0cac94f153cc3919ccfed3f4efe2dfb
JulBeet <julbeet@gmail.com>
2018-03-06 18:59:49 +0200

    Design markup added

--- src/index.js
+++ src/index.js
@@ -11,10 +11,87 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'Depositphotos_163608264_original1.jpg',
+  },
+  title: {
+    content: 'About us',
+    type: 'blockTitle',
+  },
+  heading: {
+    content: 'Heading about us',
+    type: 'heading',
+  },
+  subheading: {
+    content: 'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
+    type: 'subtitle',
+    brightness: 70,
+  },
+  text: {
+    content: 'We are in it for the long haul—for our customers and for our world. Our customers can be found in virtually every corner of the earth, and we realize our success comes directly from helping our customers be successful.',
+    type: 'text',
+    brightness: 70,
+  },
+  'social-icons': {
+    networks: [
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 5,
+      color: 'var(--ui-light-shade-color)',
+      sizes: [10, 20, 30, 40],
+      size: 23,
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  align: {
+    children: [
+      {id: 'left', label: 'Left'},
+      {id: 'center', label: 'Center'},
+      {id: 'right', label: 'Right'},
+    ],
+    defaultValue: 'left',
+    name: 'Aligning',
+    type: 'radio-button-group',
+  },
+  heading: {defaultValue: true, label: 'Company title', type: 'checkbox'},
+  subheading: {defaultValue: true, label: 'Company description', type: 'checkbox'},
+  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
+  'social-icons': {defaultValue: true, label: 'Social media buttons', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
+  'additional-button': {defaultValue: true, label: 'Additional button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
