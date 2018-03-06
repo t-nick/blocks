28c2dfe935ce680cfebbf5e80effa7924fd2528c
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-03-06 10:37:50 -0800

    done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,87 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Reasearch',
+        type: 'heading',
+      },
+      text: {
+        content: 'The Emerald Buddha is a figurine sitting Budha, that is the is the palladium of the Kingdom of Thailand.',
+        type: 'text',
+      },
+      picture: {
+        src: 'image1.jpg',
+        alt: 'Quantum E-commerce illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn More',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Development',
+        type: 'heading',
+      },
+      text: {
+        content: 'The Emerald Buddha is a figurine sitting Budha, that is the is the palladium of the Kingdom of Thailand.',
+        type: 'text',
+      },
+      picture: {
+        src: 'image2.jpg',
+        alt: 'Quantum Aftersales illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn More',
+        type: 'primary',
+      },
+    },
+  ],
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'More services',
+    type: 'secondary',
+  },
+
+  background: {
+    type: 'gradient',
+    gradient: {
+      angle: 90,
+      firstColor: '#3f4953',
+      secondColor: '#2e3336',
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
