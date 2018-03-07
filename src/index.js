0ddc6c9ae1e693b6387bd5bd92c705eadbd5ed6d
nataliadiadkova <nataliadiadkova@gmail.com>
2018-03-07 17:12:30 +0200

    design

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ExtendedWireframe.defaultContent = {
   },
   subheading: {
     content: 'Learn to skydive with the experienced instructors at SkydiveBirds club near Chicago!',
-    type: 'subheading',
+    type: 'heading',
   },
   text: {
     content: 'SkydiveBirds Center is proud to offer the Accelerated Freefall (AFF) training program for those who want to improve their skills and become qualified, licensed skydivers. AFF is a quick and easy way to learn to skydive. You will learn to correctly get out of the plane, remain conscious of altitude, freefall in a fixed position, execute maneuvers, deploy from a fixed position at the right time, estimate and respond to an emergency, fly under the canopy, and make a safe landing. You will learn this art through 7 special jumps with the instructor nearby you, who will teach you every step of the way, from 12,000 feet.',
