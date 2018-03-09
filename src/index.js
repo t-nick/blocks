7ced305f35116ee68671bc3f5388df14f6f8ec44
max-sindi <britva.okkamaa@gmail.com>
2018-03-09 18:57:19 +0200

    design-code structure changed

--- src/index.js
+++ src/index.js
@@ -11,6 +11,77 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'State of the art Skydiving Aircraft',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content:
+      'We have a fleet of 14-seat Cessna and 24-seat Twin Otter Caravans which are fully insured, safe, professionally maintained, and economic for motion picture projects. We also have such aircraft as load-lifting and A-Star copters, vintage planes Beech 18, DC-3, and more.',
+    type: 'subtitle',
+  },
+  services: [
+    {
+      ...Component.defaultContent.services[0],
+      title: {
+        content: 'DeHavilland DHC-3 Otter',
+        type: 'heading',
+      },
+      description: {
+        content: 'May 20, 2009',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'service-1.png',
+        alt: 'Service illustration photo',
+      },
+    },
+    {
+      ...Component.defaultContent.services[0],
+      title: {
+        content: 'Cessna 208 Caravan',
+        type: 'heading',
+      },
+      description: {
+        content: 'May 22, 2009',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'service-2.png',
+        alt: 'Service illustration photo',
+      },
+    },
+    {
+      ...Component.defaultContent.services[0],
+      title: {
+        content: 'Cessna 182 Skylane',
+        type: 'heading',
+      },
+      description: {
+        content: 'May 23, 2009',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'service-3.png',
+        alt: 'Service illustration photo',
+      },
+    },
+    {
+      ...Component.defaultContent.services[0],
+      title: {
+        content: 'Cessna 182 G',
+        type: 'heading',
+      },
+      description: {
+        content: 'May 23, 2009',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'service-4.png',
+        alt: 'Service illustration photo',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
