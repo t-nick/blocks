684b0efa66ecee69a17a899b338a996284692530
nataliadiadkova <nataliadiadkova@gmail.com>
2018-03-12 15:52:54 +0200

    design

--- src/index.js
+++ src/index.js
@@ -277,10 +277,6 @@ ExtendedWireframe.defaultContent = {
     content: 'Skydiving instructors',
     type: 'blockTitle',
   },
-  subtitle: {
-    content: 'Our team is our proud. Each of the team members is dedicated to delivering the best results to the clients. Meet some of our expert who will work on your case:',
-    type: 'subtitle',
-  },
   cta: {
     actionConfig: {
       action: 'link',
@@ -294,10 +290,6 @@ ExtendedWireframe.defaultContent = {
     },
     textValue: 'Meet our team',
     type: 'secondary',
-  },
-  topIcon: {
-    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
-    fill: 'red',
   },
 }
 
