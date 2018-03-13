7f0dacece8f905a29b73277b98dc37c5fcca1b4f
isergienko <igorserhiyenko@gmail.com>
2018-03-13 17:57:11 +0200

    services without bg

--- src/index.js
+++ src/index.js
@@ -11,10 +11,84 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'First Services',
+        type: 'heading',
+      },
+      text: {
+        content: 'The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.',
+        type: 'text',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Try now',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Second Services',
+        type: 'heading',
+      },
+      text: {
+        content: 'The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.',
+        type: 'text',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Try now',
+        type: 'primary',
+      },
+    },
+  ],
+  title: {
+    content: '- Services -',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?',
+    type: 'subtitle',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+  },
 }
+
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Service description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
