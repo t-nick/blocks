65beb67847e5bb848dc5f8388995010835900e1f
tangerine69 <mrtangerine69@gmail.com>
2018-03-14 11:18:50 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,199 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  events: [
+    {
+      title: {
+        content: 'Making A New Trend In Poster',
+        type: 'heading',
+      },
+      date: {
+        content: '27 Aug 2018',
+        type: 'text',
+      },
+      badge: {
+        content: 'members only',
+        type: 'caption',
+      },
+      time: {
+        content: '6:30 pm - 7:30 pm',
+        type: 'text',
+      },
+      location: {
+        content: 'The station',
+        type: 'caption',
+      },
+      picture: {
+        resourceRef: 'events_01.jpg',
+        alt: 'Digital marketing workshop illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Making A New Trend In Poster',
+        type: 'heading',
+      },
+      date: {
+        content: '27 Aug 2018',
+        type: 'text',
+      },
+      badge: {
+        content: 'members only',
+        type: 'caption',
+      },
+      time: {
+        content: '6:30 pm - 7:30 pm',
+        type: 'text',
+      },
+      location: {
+        content: 'The station',
+        type: 'caption',
+      },
+      picture: {
+        resourceRef: 'events_02.jpg',
+        alt: 'Digital marketing workshop illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Making A New Trend In Poster',
+        type: 'heading',
+      },
+      date: {
+        content: '27 Aug 2018',
+        type: 'text',
+      },
+      badge: {
+        content: 'members only',
+        type: 'caption',
+      },
+      time: {
+        content: '6:30 pm - 7:30 pm',
+        type: 'text',
+      },
+      location: {
+        content: 'The station',
+        type: 'caption',
+      },
+      picture: {
+        resourceRef: 'events_03.jpg',
+        alt: 'Digital marketing workshop illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Making A New Trend In Poster',
+        type: 'heading',
+      },
+      date: {
+        content: '27 Aug 2018',
+        type: 'text',
+      },
+      badge: {
+        content: 'members only',
+        type: 'caption',
+      },
+      time: {
+        content: '6:30 pm - 7:30 pm',
+        type: 'text',
+      },
+      location: {
+        content: 'The station',
+        type: 'caption',
+      },
+      picture: {
+        resourceRef: 'events_04.jpg',
+        alt: 'Digital marketing workshop illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+  ],
+  title: {
+    content: '- Events -',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.',
+    type: 'subtitle',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: '— All Questions —',
+    type: 'primary',
+  },
+  background: {
+    type: 'color',
+    color: 'light-accent-color',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: "Event's description", type: 'checkbox'},
 }
 
 export default ExtendedWireframe
