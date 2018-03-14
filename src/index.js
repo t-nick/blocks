a20a383680c209fd370bf5b2d0bb16e4ffa2d51b
Vladimir Zelik <kazantip81v@gmail.com>
2018-03-15 00:19:11 +0200

    add style & content

--- src/index.js
+++ src/index.js
@@ -11,6 +11,84 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Our licenses',
+    type: 'blockTitle',
+    color: 'var(--ui-block-title-color)',
+  },
+  awards: [
+    {
+      title: {
+        content: 'C-10 License on any electrical work',
+        type: 'heading',
+        color: 'var(--ui-heading-color)',
+        fontSize: 'var(--ui-heading-size )',
+      },
+      picture: {
+        resourceRef: 'battle-certificate-418.png',
+        alt: 'C-10 License on any electrical work',
+      },
+    },
+    {
+      title: {
+        content: 'C-7 Electrical work for low voltage',
+        type: 'heading',
+        color: 'var(--ui-heading-color)',
+      },
+      picture: {
+        resourceRef: 'battle-certificate-292.png',
+        alt: 'C-7 Electrical work for low voltage',
+      },
+    },
+    {
+      title: {
+        content: 'C-45 Electrical work for electrical sign',
+        type: 'heading',
+        color: 'var(--ui-heading-color)',
+      },
+      picture: {
+        resourceRef: 'battle-certificate-223.png',
+        alt: 'C-45 Electrical work for electrical sign',
+      },
+    },
+    {
+      title: {
+        content: 'C-46 License on electrical work for solar systems',
+        type: 'heading',
+        color: 'var(--ui-heading-color)',
+      },
+      picture: {
+        resourceRef: 'battle-certificate-362.png',
+        alt: 'C-46 License on electrical work for solar systems',
+      },
+    },
+    {
+      title: {
+        content: 'C-28 Electrical work for garage door',
+        type: 'heading',
+        color: 'var(--ui-heading-color)',
+      },
+      picture: {
+        resourceRef: 'battle-certificate-311.png',
+        alt: 'C-28 Electrical work for garage door',
+      },
+    },
+    {
+      title: {
+        content: 'C-34 Electrical work for prefabricated office workstation',
+        type: 'heading',
+        color: 'var(--ui-heading-color)',
+      },
+      picture: {
+        resourceRef: 'battle-certificate-543.png',
+        alt: 'C-34 Electrical work for prefabricated office workstation',
+      },
+    },
+  ],
+  background: {
+    type: 'color',
+    color: 'var(--ui-light-accent-color)',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
