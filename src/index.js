6b9a680b734b681afc529816c0543337e873b802
nataliadiadkova <nataliadiadkova@gmail.com>
2018-03-14 15:38:25 +0200

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,43 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'electrical-bg.png',
+  },
+  title: {
+    content: 'Meeting Any  Electrical Needs',
+    type: 'heroTitle',
+  },
+  subtitle: {
+    content: 'We serve residential, commercial, and industrial clients',
+    type: 'subtitle',
+    color: 'light-shade-color',
+  },
+  'cta-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'make an order',
+    type: 'primary',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'secondary-btn': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
