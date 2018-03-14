b2e475a0335b3da86ee06531bec9a77fa0a88e05
Slipachenko <slipachenko@Mac-mini-Monster-2.local>
2018-03-14 11:44:44 +0200

    change style and index

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ ExtendedWireframe.defaultContent = {
     },
   },
   topIcon: {
-    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"><path d="M20 13.473V1.059l-6.896 6.207zM10 10.06L8.031 8.288l-6.896 6.207h17.73l-6.896-6.207zM18.865.037H1.135L10 8.017zM0 1.059v12.414l6.896-6.207z"/></svg>',
     fill: 'brand-color',
   },
 }
