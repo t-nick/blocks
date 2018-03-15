89a00d7252644b790680c3d05b26333c0c8d5b67
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-03-15 15:11:19 +0200

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,6 +11,15 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Information',
+  },
+  text: {
+    ...Component.defaultContent.text,
+    content: 'For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words.<br><br> If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental.',
+    color: 'var(--ui-dark-accent-color)',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
