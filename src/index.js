7785bbf7991bfcc30a81e7a075951ac60a5ed185
tangerine69 <mrtangerine69@gmail.com>
2018-03-15 17:10:48 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,53 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#d8d8d8',
+  },
+  formBackground: {
+    type: 'color',
+    color: 'light-shade-color',
+  },
+  title: {
+    content: 'Want to work with us?',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Join 13000 clients who already worked with us. Request a quote to get best solutions.',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: '',
+        placeholder: 'Name *',
+        required: true,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: '',
+        placeholder: 'Phone *',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: '',
+        placeholder: 'Email',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Sign Up Now',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
