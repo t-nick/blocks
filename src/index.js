5a6398f0d16c207a3e24c2152aa28f4aa34dab85
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-03-16 15:58:30 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,123 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Contacts',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'When no fees are being charged at the exchange, it is possible for a trader (or bot) to trade back and forth with themselves.',
+    type: 'subtitle',
+  },
+  contacts: [
+    {
+      title: {
+        content: 'Head office Berlin',
+        type: 'heading',
+      },
+      address: {
+        content: '0500 Hagenes Haven Apt. 718',
+        type: 'text',
+      },
+      phone: {
+        content: '<a href="tel:1 (222) 123-12-12">1 (222) 123-12-12</a>',
+        type: 'text',
+      },
+      email: {
+        content: '<a href="mailto:junius_price@raynor.biz">junius_price@raynor.biz</a>',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'contacts_01.jpg',
+        alt: 'Location illustration photo',
+      },
+
+    },
+    {
+      title: {
+        content: 'New York office',
+        type: 'heading',
+      },
+      address: {
+        content: '4946 Aglae Cliff Suite 302',
+        type: 'text',
+      },
+      phone: {
+        content: '<a href="tel:1 (222) 123-12-12">1 (222) 123-12-12</a>',
+        type: 'text',
+      },
+      email: {
+        content: '<a href="mailto:clovis_rogahn@joy.us">clovis_rogahn@joy.us</a>',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'contacts_02.jpg',
+        alt: 'Location illustration photo',
+      },
+    },
+    {
+      title: {
+        content: 'London office',
+        type: 'heading',
+      },
+      address: {
+        content: '196 Erdman Causeway Apt. 829',
+        type: 'text',
+      },
+      phone: {
+        content: '<a href="tel:1 (222) 123-12-12">1 (222) 123-12-12</a>',
+        type: 'text',
+      },
+      email: {
+        content: '<a href="mailto:fanny_denesik@yahoo.com">fanny_denesik@yahoo.com</a>',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'contacts_03.jpg',
+        alt: 'Location illustration photo',
+      },
+    },
+  ],
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+    size: 'lg',
+  },
+  'button-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'CONTACT US',
+    type: 'primary',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Contacts description', type: 'checkbox'},
+  phone: {defaultValue: true, label: 'Phone', type: 'checkbox'},
+  email: {defaultValue: true, label: 'E-mail', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
+  'button-secondary': {defaultValue: true, label: 'Additional button', type: 'checkbox'},
+  'top-caption': {defaultValue: false, label: 'Top caption', type: 'hidden'},
 }
 
 export default ExtendedWireframe
