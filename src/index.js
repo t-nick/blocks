677ec3f08840baa55a6b872f0f462af065a202b3
max-sindi <britva.okkamaa@gmail.com>
2018-03-16 20:00:25 +0200

    design is done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,83 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    resourceRef: 'bg.png',
+  },
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Latest projects',
+  },
+  gallery: [
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        resourceRef: 'item-1.png',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        resourceRef: 'item-2.png',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        resourceRef: 'item-3.png',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        resourceRef: 'item-4.png',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        resourceRef: 'item-5.png',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        resourceRef: 'item-6.png',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+    },
+  ],
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'view all projects',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'secondary-button': {defaultValue: true, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
