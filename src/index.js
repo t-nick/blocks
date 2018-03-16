d4f0a882ebc7efc04b26bf83a1e09e5e8d16ac16
Vladimir Zelik <kazantip81v@gmail.com>
2018-03-16 10:50:26 +0200

    add style & content

--- src/index.js
+++ src/index.js
@@ -11,6 +11,57 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'pic.png',
+  },
+  formBackground: {
+    type: 'color',
+    color: 'light-shade-color',
+  },
+  title: {
+    content: 'Want to work with us ?',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Join 13000 clients who already worked with us. Request a quote to get best solutions.',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Enter your name',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: 'Enter your phone',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Enter your email',
+        placeholder: '',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Request a quote',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
