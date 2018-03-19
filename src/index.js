f4c3c3cc96309f7f0ff2885b79e21d0514d57076
tangerine69 <mrtangerine69@gmail.com>
2018-03-19 19:02:03 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,128 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Municipalities',
+        type: 'heading',
+      },
+      text: {
+        content: 'Mixed Use, Civic, Fire',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'services_img-1.png',
+        alt: 'Municipalities illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Research Labs',
+        type: 'heading',
+      },
+      text: {
+        content: ' ',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'services_img-2.png',
+        alt: 'Quantum Aftersales illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Special Use',
+        type: 'heading',
+      },
+      text: {
+        content: 'Military, Courthouses, Detention Centres',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'services_img-3.jpg',
+        alt: 'Quantum Aftersales illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+  ],
+  title: {
+    content: 'What we clean',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Do you want to sell and promote your business? Here are best of Quantum services for you: ',
+    type: 'subtitle',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+  },
+  'top-caption': {
+    content: 'metus et hendrerit aliquet',
+    type: 'text',
+  },
+  background: {
+    type: 'color',
+    color: '#fff',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'service-button': {defaultValue: false, label: 'Service button', type: 'checkbox'},
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
