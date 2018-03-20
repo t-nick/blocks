96c3c9971e04b5116d51443bbff437dfb2256e50
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-03-20 09:43:50 -0700

    done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,107 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Multi-Tenant Office Buildings',
+        type: 'heading',
+      },
+      text: {
+        content: 'Multipurpose program that will perfectly suit online stores and online retail business. You can add up to 2000 items with a detailed description, characteristics, and photos. Also, the shopping cart, clients’ feedback, rating, and related items option are available.',
+        type: 'text',
+      },
+      picture: {
+        src: 'image1.jpg',
+        alt: 'Quantum E-commerce illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Single-Tenant Buildings',
+        type: 'heading',
+      },
+      text: {
+        content: 'Multipurpose program that will perfectly suit online stores and online retail business. You can add up to 2000 items with a detailed description, characteristics, and photos. Also, the shopping cart, clients’ feedback, rating, and related items option are available.',
+        type: 'text',
+      },
+      picture: {
+        src: 'image2.jpg',
+        alt: 'Quantum E-commerce illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Owner-Occupied Buildings',
+        type: 'heading',
+      },
+      text: {
+        content: 'Aftersales assistance that includes marketing campaigns, additional digital solutions, and educational courses. Our specialists will help to find out what will work for your business and decide on the steps that are to be taken immediately. ',
+        type: 'text',
+      },
+      picture: {
+        src: 'image3.jpg',
+        alt: 'Quantum Aftersales illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+  ],
+  title: {
+    content: 'What we clean',
+    type: 'blockTitle',
+  },
+  background: {
+    type: 'color',
+    color: '#fff',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'body-text': {defaultValue: false, label: 'Service main text', type: 'checkbox'},
+  'service-button': {defaultValue: false, label: 'Service button', type: 'checkbox'},
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
