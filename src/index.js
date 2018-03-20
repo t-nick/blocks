4755b7e3fb18e10ffdaea6b9db106dec566769e9
Ostafiichuk <v.ostafiichuk1995@gmail.com>
2018-03-20 06:57:41 -0700

    done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,33 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'image1.jpg',
+  },
+  title: {
+    content: 'Adaline',
+    type: 'blockTitle',
+  },
+  text: {
+    content: 'Adaline was a waitress and a hotel housekeeper in Poland. Then she worked  in NYC as a part of a team that serviced apartments. Her eight years of  practical experience and high scale standards have led her to the  ShinyHouse team. Adaline is dedicated to satisfying any clients\' needs and  requirements.',
+    type: 'text',
+  },
+  subheading: {
+    content: '“The house was ideally clean. I\'m completely satisfied with the result!” - Sam Jackson',
+    type: 'subheading',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'social-icons': {defaultValue: false, label: 'Social media buttons', type: 'checkbox'},
+  'additional-button': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
+  subheading: {defaultValue: true, label: 'Company description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
