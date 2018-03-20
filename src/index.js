709d7e8ab0316d4f5e1a63ced5bdd219f168cfde
VladReznik <vlad_escape@ukr.net>
2018-03-20 17:00:25 +0200

    first

--- src/index.js
+++ src/index.js
@@ -8,13 +8,119 @@ import options from './options.json'
 const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
-
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Window Cleaning',
+        type: 'heading',
+      },
+      text: {
+        content: 'From $37',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'back_1.png',
+        alt: 'Quantum E-commerce illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn More',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Carpet Cleaning',
+        type: 'heading',
+      },
+      text: {
+        content: 'From $27',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'back_2.png',
+        alt: 'Quantum Aftersales illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Upholstery Cleaning',
+        type: 'heading',
+      },
+      text: {
+        content: 'From $6',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'back_3.png',
+        alt: 'Quantum Aftersales illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'primary',
+      },
+    },
+  ],
+  title: {
+    content: 'Combine Office Cleaning with',
+    type: 'blockTitle',
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'More services',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'body-text': {defaultValue: true, label: 'Service main text', type: 'checkbox'},
+  'service-button': {defaultValue: true, label: 'Service button', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
