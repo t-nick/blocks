bf7576e966a02095f7457c782195af1d1a249f2c
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-03-21 12:41:49 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,111 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#fafafa',
+  },
+  'top-caption': {
+    content: 'metus et hendrerit aliquet',
+    type: 'text',
+  },
+  title: {
+    content: 'The Special Featured Services',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Morbi nec metus. Phasellus blandit leo ut odio. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem.',
+    type: 'subtitle',
+  },
+  services: [
+    {
+      background: {
+        type: 'gradient',
+        gradient: {
+          angle: 90,
+          firstColor: '#3657f1',
+          secondColor: '#235ff7',
+        },
+      },
+      title: {
+        content: 'Product Design',
+        type: 'heading',
+      },
+      text: {
+        content: 'Fusce convallis metus id felis luctus adipiscing. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue. <br><br>Ac auctor orci leo non est. Quisque id mi. Ut tincidunt tincidunt erat. Etiam feugiat lorem.',
+        type: 'text',
+      },
+      picture: {
+        src: 'https://weblium-prod.storage.googleapis.com/res/5a4b747cd294a10025a2a530/5a4baf9cd294a10025a2b305.png',
+        alt: 'Quantum E-commerce illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Show More',
+        type: 'secondary',
+      },
+    },
+    {
+      title: {
+        content: 'Market Strategy',
+        type: 'heading',
+      },
+      text: {
+        content: 'Praesent egestas neque eu enim. In hac habitasse platea dictumst. Fusce a quam. Etiam ut purus mattis mauris sodales aliquam.<br><br>Curabitur nisi. Quisque malesuada placerat nisl. Nam ipsum risus, rutrum vitae, vestibulum.',
+        type: 'text',
+      },
+      picture: {
+        src: 'https://weblium-prod.storage.googleapis.com/res/5a4b747cd294a10025a2a530/5a4baf9cd294a10025a2b305.png',
+        alt: 'Quantum Aftersales illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Show more',
+        type: 'secondary',
+      },
+    },
+  ],
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Show more',
+    type: 'primary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Service description', type: 'checkbox'},
+  'body-text': {defaultValue: true, label: 'Service main text', type: 'checkbox'},
+  'service-button': {defaultValue: true, label: 'Service button', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
+  'top-caption': {defaultValue: true, label: 'Top caption', type: 'hidden'},
 }
 
 export default ExtendedWireframe
