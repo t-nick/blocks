c91834997f387acc09f67bbda13d00da4c2f6f3b
KonstantinKH <kkhomenko9@gmail.com>
2018-03-22 11:03:58 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -11,6 +11,88 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  cover: [
+    {
+      title: {
+        content: 'Perfect Choice',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: '25 years of business achievements',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Read More',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Perfect Choice',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: '25 years of business achievements',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Read More',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Perfect Choice',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: '25 years of business achievements',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Read More',
+        type: 'primary',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
