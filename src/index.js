bb11bb0d2c7d77384aa02ae138fadf2b8992770a
JulBeet <julbeet@gmail.com>
2018-03-22 16:40:10 +0200

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,102 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'cover.jpg',
+    overlay: {
+      type: 'color',
+      color: 'var(--ui-light-shade-color)',
+      opacity: 0.7,
+    },
+  },
+  cover: [
+    {
+      title: {
+        content: 'The only journey is the one within',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We implement Innovative Projects',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'SIGN UP',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Quantum Company',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We implement Innovative Projects',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Quantum Company',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We implement Innovative Projects',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+  ],
+
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  arrows: {defaultValue: true, label: 'Navigation arrows', type: 'checkbox'},
+  dots: {defaultValue: false, label: 'Navigation indicators', type: 'checkbox'},
+  'primary-button': {defaultValue: true, label: 'Button', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
