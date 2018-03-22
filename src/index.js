b208a1224f2d134b456affc4b9698cd3cca20de0
tangerine69 <mrtangerine69@gmail.com>
2018-03-22 15:08:09 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-lucario-cover/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,6 +11,88 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  cover: [
+    {
+      title: {
+        content: 'Type Something',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'Type Something',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Main button (L)',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Type Something',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'Type Something',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Main button (L)',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Type Something',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'Type Something',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Main button (L)',
+        type: 'primary',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
