8ad17c54ab3ce31a120de8738e84ea0836007872
VladReznik <vlad_escape@ukr.net>
2018-03-23 12:35:27 +0200

    First Push

--- src/index.js
+++ src/index.js
@@ -11,10 +11,65 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'background.png',
+  },
+  title: {
+    content: 'Move In/Move Out Cleaning',
+    type: 'blockTitle',
+  },
+  heading: {
+    content: 'A whole bulk of services',
+    type: 'heading',
+  },
+  subheading: {
+    content: '— Free re-clean within 72 hours if needed, <br> — Short-notice bookings available, <br> — Following checklist approved by estate agencies, <br> — Deep oven cleaning, equipment, and detergents included',
+    type: 'subheading',
+  },
+  text: {
+    content: 'From $95 ',
+    type: 'text',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  align: {
+    children: [
+      {id: 'left', label: 'Left'},
+      {id: 'center', label: 'Center'},
+      {id: 'right', label: 'Right'},
+    ],
+    defaultValue: 'left',
+    name: 'Aligning',
+    type: 'radio-button-group',
+  },
+  heading: {defaultValue: false, label: 'Company title', type: 'checkbox'},
+  subheading: {defaultValue: true, label: 'Company description', type: 'checkbox'},
+  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
+  'social-icons': {defaultValue: false, label: 'Social media buttons', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
+  'additional-button': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
+  'top-caption': {defaultValue: false, label: 'Top caption', type: 'hidden'},
 }
 
 export default ExtendedWireframe
