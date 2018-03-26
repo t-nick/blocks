ab8b83ddc61f9153a019ebe18a601f9a27efbfdd
efimenko bohdan <bogdane@weblium.com>
2018-03-26 15:37:38 +0300

    first iteration

--- src/component.js
+++ src/component.js
@@ -7,24 +7,29 @@ class Wireframe extends React.Component {
   }
 
   render() {
-    const {components: {Text}, style} = this.props
+    const {components: {Text, Button}, style} = this.props
 
     return (
       <section className={style.section}>
         <div className={style.section__inner}>
           <Text bind="title" className={style.title} tagName="h1" />
           <Text bind="description" className={style.description} tagName="p" />
-
-          <button type="button" className={style.button}>Main button (M)</button>
+          <div className={style['button-wrapper']}>
+            <Button bind="button" buttonClassName={style.button} />
+          </div>
         </div>
       </section>
     )
   }
 }
 
-Wireframe.components = _.pick(['Text'])($editor.components)
+Wireframe.components = _.pick(['Text', 'Button'])($editor.components)
 
 Wireframe.defaultContent = {
+  background: {
+    type: 'color',
+    color: '#d8d8d8',
+  },
   title: {
     content: 'About us',
     type: 'blockTitle',
@@ -32,6 +37,10 @@ Wireframe.defaultContent = {
   description: {
     content: 'We provide a whole bulk of services to ensure stable growth of the company. The profound expertise of our team, deep understanding of all business aspects, ideal knowledge of processes will help to deal with various issues.',
     type: 'text',
+  },
+  button: {
+    textValue: 'Main button (M)',
+    type: 'primary',
   },
 }
 
