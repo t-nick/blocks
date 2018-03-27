da6d496d05a5fa9e8aec34ea13a9b21987edf02a
KonstantinKH <kkhomenko9@gmail.com>
2018-03-27 15:21:53 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -11,10 +11,88 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  numbers: [
+    {
+      title: {
+        content: 'Increased profits by',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Experience in Transportation',
+        type: 'text',
+      },
+      value: {
+        content: '32 yrs',
+        type: 'blockTitle',
+      },
+    },
+    {
+      title: {
+        content: 'Expanded market reach by',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Offices and Logistics Facilities',
+        type: 'text',
+      },
+      value: {
+        content: '1000',
+        type: 'blockTitle',
+      },
+    },
+    {
+      title: {
+        content: 'Improved customer satisfaction by',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Skilled Drivers in Our Fleet',
+        type: 'text',
+      },
+      value: {
+        content: '22,000',
+        type: 'blockTitle',
+      },
+    },
+    {
+      title: {
+        content: 'Improved customer satisfaction by',
+        type: 'subtitle',
+      },
+      label: {
+        content: 'Miles Driven Per Year',
+        type: 'text',
+      },
+      value: {
+        content: '2 Billion',
+        type: 'blockTitle',
+      },
+    },
+  ],
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'More About Us',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'number-title': {
+    defaultValue: false,
+    label: 'Additional information on numbers',
+    type: 'checkbox',
+  },
+  title: {defaultValue: false},
 }
 
 export default ExtendedWireframe
