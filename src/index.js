5d559654daa8e617cb8d53b53076425fad23d5e1
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-03-27 15:45:49 +0300

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,23 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Our locations',
+  },
+  map: {
+    ...Component.defaultContent.map,
+    preset: 'dark',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  address: {defaultValue: false, label: 'Address text block', type: 'checkbox'},
+  email: {defaultValue: false, label: 'E-mail text block', type: 'checkbox'},
+  logo: {defaultValue: false, label: 'Logo', type: 'checkbox'},
+  phone: {defaultValue: false, label: 'Phone text block', type: 'checkbox'},
+  social: {defaultValue: false, label: 'Social Media Buttons', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
