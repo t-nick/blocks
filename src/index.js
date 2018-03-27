97723d435f85593a7f87365cfbbd9a61c09a23a8
tangerine69 <mrtangerine69@gmail.com>
2018-03-27 20:23:21 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,280 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Transportation Management',
+        type: 'heading',
+      },
+      description: {
+        content: 'Multipurpose assistance for online stores and offline retail businesses.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: '01.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Supply Chain Analysis and Design',
+        type: 'heading',
+      },
+      description: {
+        content: 'Developing marketing and business strategy to help company grow fast.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: '02.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'White Glove Pick-Up & Delivery',
+        type: 'heading',
+      },
+      description: {
+        content: 'Building brand awareness for micro and small businesses to increase potential reach.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: '03.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Freight Audit & Payment',
+        type: 'heading',
+      },
+      description: {
+        content: 'Aftersales support in marketing, sales, and staff training for enhanced performance.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: '04.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Customs Brokerage',
+        type: 'heading',
+      },
+      description: {
+        content: 'Aftersales support in marketing, sales, and staff training for enhanced performance.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: '05.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Warehousing',
+        type: 'heading',
+      },
+      description: {
+        content: 'Aftersales support in marketing, sales, and staff training for enhanced performance.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: '06.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Container Drayage',
+        type: 'heading',
+      },
+      description: {
+        content: 'Aftersales support in marketing, sales, and staff training for enhanced performance.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: '07.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Last Mile Logistics',
+        type: 'heading',
+      },
+      description: {
+        content: 'Aftersales support in marketing, sales, and staff training for enhanced performance.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: '08.png',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+  ],
+  topIcon: {
+    svg:
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
+  title: {
+    content: 'Logistics Solutions',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'We deliver all kind of services that support small and micro businesses. Here are some of them:',
+    type: 'subtitle',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+  },
+  'button-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'button-secondary': {defaultValue: false, label: 'Button', type: 'checkbox'},
+  link: {defaultValue: false, label: 'Link', type: 'checkbox'},
+  body: {defaultValue: false, label: 'Service main text', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
