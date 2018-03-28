a51d132ae0582c26febca9c69897bf2945a1494f
JulBeet <julbeet@gmail.com>
2018-03-28 10:51:05 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,42 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Join Acrozz Team',
+    type: 'blockTitle',
+  },
+  text: {
+    content: 'Our company is always looking for experienced people who want to become a part of our team. We are truly dedicated to safety, service, and satisfaction.  ',
+    type: 'heading',
+  },
+  'text-2': {
+    content: 'So make a wise choice!',
+    type: 'small',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'View Positions',
+    type: 'secondary',
+    size: 'md',
+  },
+  picture: {
+    src: 'Depositphotos_47582699_original.jpg',
+    alt: 'Picture about the company',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'additional-text': {defaultValue: true, label: 'Additional text', type: 'hidden'},
 }
 
 export default ExtendedWireframe
