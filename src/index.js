29dc0bdaaef6fc7899fa7960d80019a32c095535
JulBeet <julbeet@gmail.com>
2018-03-28 16:59:35 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,6 +11,245 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  logo: {
+    text: {
+      value: 'Roy Hale',
+      fontSize: 30,
+    },
+  },
+  description: {
+    type: 'text',
+    content: 'Helping businesses <br/>grow & prosper',
+  },
+  menu: [
+    {
+      id: 'about',
+      metadata: {
+        displayName: 'About',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'services',
+      metadata: {
+        displayName: 'Services',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'resources',
+      metadata: {
+        displayName: 'Resources',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'testimonials',
+      metadata: {
+        displayName: 'Testimonials',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'coaching',
+      metadata: {
+        displayName: 'Coaching',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'books',
+      metadata: {
+        displayName: 'Books',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'blog',
+      metadata: {
+        displayName: 'Blog',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'Courses',
+      metadata: {
+        displayName: 'Courses',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'videos',
+      metadata: {
+        displayName: 'Videos',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'speaking',
+      metadata: {
+        displayName: 'Speaking',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'workshops',
+      metadata: {
+        displayName: 'Workshops',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'podcasts',
+      metadata: {
+        displayName: 'Podcasts',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+  ],
+  socialIcons: {
+    networks: [
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: false,
+      preset: 'preset001',
+      offset: 2,
+      color: '#9b9b9b',
+      sizes: [10, 20, 30, 40],
+      size: 37,
+    },
+  },
+  copyright: {
+    type: 'caption',
+    content: 'Produced by Weblium.com. All rights Reserved',
+  },
+  additional: {
+    type: 'caption',
+    content: 'Demo Preview for Roy Hale Business Coach Concept',
+  },
+  background: {
+    type: 'color',
+    color: 'var(--ui-dark-shade-color )',
+  },
+
 }
 
 ExtendedWireframe.modifierScheme = {
