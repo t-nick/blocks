e1768fc53ac7fc4d1a7d12ea1fcdc89649f094f2
Sabina <sabina.mamedova90@gmail.com>
2018-03-28 14:57:24 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -10,11 +10,291 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+  title: {
+    content: 'Partners',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Acrozz collaborates with a diverse range of business partners to deliver first-grade services and technologies to our customers. We also support supply chain programs for several main universities, sharing our experience at events and staying in close connection with logistics professionals',
+    type: 'subtitle',
+  },
+  partners: [
+    {
+      logo: {
+        image: {
+          resourceRef: 'man.jpg',
+          width: 270,
+          height: 270,
+        },
+      },
+      title: {
+        content: 'MAN',
+        type: 'heading',
+      },
+      desc: {
+        content: 'Back in 1995, we started cooperation with this brand. Samsung became our main supplier of high-quality tech products. Today, Samsung is our main partner who supports each initiative and actively participates in company development.',
+        type: 'text',
+      },
+      button: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Partner’s website',
+        type: 'link',
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'daf.jpg',
+          width: 270,
+          height: 270,
+        },
+      },
+      title: {
+        content: 'Daf ',
+        type: 'heading',
+      },
+      desc: {
+        content: 'The company that has always put reliability first. We started to work with Ford in 2002, and now this is one of the partners who’ve been with us for more than 15 years! We believe that this partnership is one of the achievements we can be proud of.',
+        type: 'text',
+      },
+      button: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Partner’s website',
+        type: 'link',
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'scania.jpg',
+          width: 270,
+          height: 270,
+        },
+      },
+      title: {
+        content: 'scania',
+        type: 'heading',
+      },
+      desc: {
+        content: 'Health of our team members is a top priority for us. Therefore, we work with the best clinics only, like Medical Family. We entrust our health to them and we entrust our business as well.',
+        type: 'text',
+      },
+      button: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Partner’s website',
+        type: 'link',
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'volvo.jpg',
+          width: 270,
+          height: 270,
+        },
+      },
+      title: {
+        content: 'volvo',
+        type: 'heading',
+      },
+      desc: {
+        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
+        type: 'text',
+      },
+      button: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Partner’s website',
+        type: 'link',
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'ford.jpg',
+          width: 270,
+          height: 270,
+        },
+      },
+      title: {
+        content: 'Ford',
+        type: 'heading',
+      },
+      desc: {
+        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
+        type: 'text',
+      },
+      button: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Partner’s website',
+        type: 'link',
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'johndeere.jpg',
+          width: 270,
+          height: 270,
+        },
+      },
+      title: {
+        content: 'john deere',
+        type: 'heading',
+      },
+      desc: {
+        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
+        type: 'text',
+      },
+      button: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Partner’s website',
+        type: 'link',
+      },
+    }, {
+      logo: {
+        image: {
+          resourceRef: 'tnt.jpg',
+          width: 270,
+          height: 270,
+        },
+      },
+      title: {
+        content: 'tnt',
+        type: 'heading',
+      },
+      desc: {
+        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
+        type: 'text',
+      },
+      button: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Partner’s website',
+        type: 'link',
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'maersk.jpg',
+          width: 270,
+          height: 270,
+        },
+      },
+      title: {
+        content: 'maersk',
+        type: 'heading',
+      },
+      desc: {
+        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
+        type: 'text',
+      },
+      button: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Partner’s website',
+        type: 'link',
+      },
+    },
+  ],
+  button: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+  },
+  topIcon: {
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
-  ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Partner description', type: 'checkbox'},
+  heading: {defaultValue: false, label: 'Partner title', type: 'checkbox'},
+  body: {defaultValue: false, label: 'About partner', type: 'checkbox'},
+  link: {defaultValue: false, label: 'Partner link', type: 'checkbox'},
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
+
 
 export default ExtendedWireframe
