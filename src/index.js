15db3972ee6311bebdf215ecb6a5b317aa616528
Vladimir Zelik <kazantip81v@gmail.com>
2018-03-28 03:35:49 +0300

    add style & content

--- src/index.js
+++ src/index.js
@@ -11,6 +11,110 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'dark-shade-color',
+  },
+
+  logo: {
+    image: {
+      resourceRef: 'layer-847.png',
+      alt: 'Logo Company',
+      width: 208,
+      height: 50,
+    },
+  },
+  menu: [
+    {
+      id: 'about-us',
+      metadata: {
+        displayName: 'About',
+      },
+    },
+    {
+      id: 'services',
+      metadata: {
+        displayName: 'Services',
+      },
+    },
+    {
+      id: 'our-team',
+      metadata: {
+        displayName: 'Our team',
+      },
+    },
+    {
+      id: 'works-gallery',
+      metadata: {
+        displayName: 'Works Gallery',
+      },
+    },
+    {
+      id: 'gift-certificates',
+      metadata: {
+        displayName: 'Gift Certificates',
+      },
+    },
+    {
+      id: 'aftercare',
+      metadata: {
+        displayName: ' Aftercare',
+      },
+    },
+    {
+      id: 'reviews',
+      metadata: {
+        displayName: 'Reviews',
+      },
+    },
+    {
+      id: 'faq',
+      metadata: {
+        displayName: 'FAQ',
+      },
+    },
+    {
+      id: 'blog',
+      metadata: {
+        displayName: 'Blog',
+      },
+    },
+    {
+      id: 'contact-us',
+      metadata: {
+        displayName: 'Contact us',
+      },
+    },
+  ],
+  socialIcons: {
+    networks: [
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 5,
+      color: '#929ca9',
+      sizes: [10, 20, 30, 40],
+      size: 40,
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
