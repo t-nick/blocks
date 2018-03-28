b74ec501b1ac42e4e5801c9d174cca5ab83faf22
tangerine69 <mrtangerine69@gmail.com>
2018-03-28 19:58:35 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,107 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'The better & safer way to get your windows cleaned',
+    type: 'blockTitle',
+  },
+  'social-title': {
+    content: 'Follow us:',
+    type: 'heading',
+  },
+  subtitle: {
+    content: 'Hire professional Window Cleaning specialists',
+    type: 'subtitle',
+  },
+  text: {
+    content: '— Done with high-reach water-fed pole system<br>— No streaks guarantee - we use only purified water<br>— Same- and next-day appointments available<br>— Technicians are certified by the Federation of Window Cleaners',
+    type: 'text',
+  },
+  'text-2': {
+    content: 'From $16/hour',
+    type: 'text',
+  },
+  picture: {
+    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+    alt: 'Picture about the company',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+    size: 'md',
+  },
+  'button-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'link',
+  },
+  socialIcons: {
+    networks: [
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'youtube',
+        name: 'YouTube',
+        url: 'http://youtube.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 15,
+      color: '#9b9b9b',
+      sizes: [10, 20, 30, 40],
+      size: 30,
+    },
+  },
+  topIcon: {
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'background.png',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'additional-text': {defaultValue: true, label: 'Additional text', type: 'hidden'},
+  'article-picture': {defaultValue: false, label: 'Article picture', type: 'hidden'},
+  subtitle: {defaultValue: true, label: 'Subtitle', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
