26a9575aab01fc40cda5a993983294e39976cb24
Kirill Donenko <kirill.donenko@gmail.com>
2018-03-28 12:58:13 +0300

    basic styles

--- src/index.js
+++ src/index.js
@@ -5,21 +5,32 @@ import style from './style.css'
 
 import options from './options.json'
 
-const {enhancers: {withProps}, hoistStatics} = $editor
+const {
+  enhancers: {
+    withProps,
+  },
+  hoistStatics,
+} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'dark-shade-color',
+  },
   logo: {
-    text: {
-      value: 'Company Logo',
-      fontSize: 24,
+    image: {
+      resourceRef: 'logo.png',
+      alt: 'LOGO',
+      width: 203, // ширина
+      height: 48, // высота
     },
   },
   description: {
     type: 'text',
-    content: 'Our company will go the extra mile to analyze the particular project in detail, and offer a business solution that will meet your requirements.',
+    content: 'Our company will go the extra mile to analyze the particular project in detail and offer a business solution that will meet your needs and requirements.',
   },
   menu: [
     {
@@ -83,69 +94,9 @@ ExtendedWireframe.defaultContent = {
       },
     },
     {
-      id: 'careers',
-      metadata: {
-        displayName: 'Careers',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'projects',
-      metadata: {
-        displayName: 'Projects',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
       id: 'blog',
       metadata: {
         displayName: 'Blog',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'approach',
-      metadata: {
-        displayName: 'Our approach',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'industries',
-      metadata: {
-        displayName: 'Industries',
         clickAction: {
           action: 'page',
           target: '_self',
@@ -173,9 +124,73 @@ ExtendedWireframe.defaultContent = {
       },
     },
     {
+      id: 'careers',
+      metadata: {
+        displayName: 'Careers',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+
+
+    {
+      id: 'approach',
+      metadata: {
+        displayName: 'Our approach',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+
+
+    {
       id: 'press',
       metadata: {
         displayName: 'Press',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'projects',
+      metadata: {
+        displayName: 'Projects',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'industries',
+      metadata: {
+        displayName: 'Industries',
         clickAction: {
           action: 'page',
           target: '_self',
@@ -208,54 +223,65 @@ ExtendedWireframe.defaultContent = {
     content: 'Follow Us',
   },
   socialIcons: {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'twitter',
-        name: 'Twitter',
-        url: 'http://twitter.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'linkedin',
-        name: 'LinkedIn',
-        url: 'http://linkedin.com/',
-      },
+    networks: [{
+      id: 'linkedin',
+      name: 'LinkedIn',
+      url: 'http://linkedin.com/',
+    },
+    {
+      id: 'twitter',
+      name: 'Twitter',
+      url: 'http://twitter.com/',
+    },
+    {
+      id: 'facebook',
+      name: 'Facebook',
+      url: 'http://facebook.com/',
+    },
+    {
+      id: 'instagram',
+      name: 'Instagram',
+      url: 'http://instagram.com/',
+    },
     ],
     target: '_blank',
     design: {
-      border: 'softRect',
+      border: null,
       innerFill: true,
       preset: 'preset001',
       offset: 15,
       color: '#9b9b9b',
       sizes: [10, 20, 30, 40],
-      size: 30,
+      size: 40,
     },
   },
   copyright: {
     type: 'caption',
-    content: 'Produced by Weblium.com.',
+    content: 'Produced by Weblium.com. All Rights Reserved',
   },
   additional: {
     type: 'caption',
-    content: 'All rights Reserved',
+    content: 'Demo Preview for Fitpower Fitness Club Concept',
   },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  title: {defaultValue: true, label: 'Company name', type: 'checkbox'},
-  description: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
-  social: {defaultValue: true, label: 'Social media Icons', type: 'checkbox'},
+  title: {
+    defaultValue: true,
+    label: 'Company name',
+    type: 'checkbox',
+  },
+  description: {
+    defaultValue: true,
+    label: 'Company main text',
+    type: 'checkbox',
+  },
+  social: {
+    defaultValue: true,
+    label: 'Social media Icons',
+    type: 'checkbox',
+  },
 }
 
 export default ExtendedWireframe
