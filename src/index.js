fc5fcc954a7f6c72cfc5cdbc1f090ad76e6ac8e1
KonstantinKH <kkhomenko9@gmail.com>
2018-03-29 10:59:59 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -11,10 +11,52 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  title: {
+    content: 'Removing Post-Construction dust',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'We’ll clean up all the mess after the construction ends.',
+    type: 'subtitle',
+  },
+  text: {
+    content: '— We send as many cleaners as needed  </br> — We use specialized equipment and detergents </br> — We restore the healthy environment of your property',
+    type: 'text',
+  },
+  'text-2': {
+    content: 'From $16/hour',
+    type: 'text',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a Quote',
+    type: 'secondary',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'article-picture': {defaultValue: false},
+  subtitle: {defaultValue: true},
+  'additional-text': {defaultValue: true},
 }
 
 export default ExtendedWireframe
