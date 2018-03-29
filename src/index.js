9f1b60ecc49867b28265b6d6bfeea162ad4cc1ea
Vladimir Zelik <kazantip81v@gmail.com>
2018-03-29 14:41:10 +0300

    add style & content

--- src/index.js
+++ src/index.js
@@ -11,10 +11,52 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'light-accent-color',
+  },
+  steps: [
+    {
+      title: {
+        content: 'Initial consultation',
+        type: 'heading',
+      },
+      description: {
+        content: 'After your first free session, I use the info you’ve given me about yourself and your business, what’s standing in your way, and what your business goals are.',
+        type: 'text',
+      },
+    },
+    {
+      title: {
+        content: 'Comprehensive assessment',
+        type: 'heading',
+      },
+      description: {
+        content: 'During the in-depth conversation about the current condition of your business and your financial potential, I’ll offer you tools to solve your causes of frustrations.',
+        type: 'text',
+      },
+    },
+    {
+      title: {
+        content: 'Getting results',
+        type: 'heading',
+      },
+      description: {
+        content: 'Through my coaching, you’ll improve your skills as an Entrepreneur and as a Manager, become the leader of your business, and achieve long-term results.',
+        type: 'text',
+      },
+    },
+  ],
+  title: {
+    content: 'How I work',
+    type: 'blockTitle',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: false, label: 'Process description', type: 'checkbox'},
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
