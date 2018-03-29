b73700d55414310fcd51395ab857e6b407e0b46b
KonstantinKH <kkhomenko9@gmail.com>
2018-03-29 12:10:07 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -11,10 +11,33 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'History',
+    type: 'blockTitle',
+  },
+  picture: {
+    resourceRef: 'image.jpg',
+    alt: 'Picture about the company',
+  },
+  subtitle: {
+    content: "Since the day of our foundation, we've been committed to attracting both newcomers and professionals to our club. Now we have a team of like-minded professionals who love to work and have fun together.",
+    type: 'subtitle',
+  },
+  text: {
+    content: '“SkydiveBirds offers a unique experience. We have mountains, we have the National Park and beautiful rivers. This is the only one location in the USA where you can be fully satisfied.”',
+    type: 'text',
+  },
+  'text-2': {
+    content: 'Stuart Bean',
+    type: 'text',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true},
+  button: {defaultValue: false},
+  'additional-text': {defaultValue: true},
 }
 
 export default ExtendedWireframe
