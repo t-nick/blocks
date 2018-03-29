3132b0c8070193f15ba193b51033f02dcb74d68d
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-03-29 12:34:04 +0300

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,91 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Ultimate',
+  },
+  products: [
+    {
+      title: {
+        content: 'Tandem flight - 7000 ft, Ultimate',
+        type: 'heading',
+      },
+      price: {
+        content: '$380',
+        type: 'headingLg',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Buy now',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Tandem flight - 10,000 ft, Ultimate',
+        type: 'heading',
+      },
+      price: {
+        content: '$460',
+        type: 'headingLg',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Buy now',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Tandem flight - 15,000 ft, Ultimate',
+        type: 'heading',
+      },
+      price: {
+        content: '$520',
+        type: 'headingLg',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Buy now',
+        type: 'primary',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  icon: {defaultValue: false, label: 'Product icon', type: 'checkbox'},
+  'price-info': {defaultValue: false, label: 'Price additional info', type: 'checkbox'},
+  body: {defaultValue: false, label: 'Product details', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
