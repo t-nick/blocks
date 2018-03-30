9e79ccd8cd4826ca3c977292e33f8a52df16b9e4
nataliadiadkova <nataliadiadkova@gmail.com>
2018-03-30 10:58:09 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,6 +11,165 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'light-shade-color',
+  },
+  logo: {
+    text: {
+      value: 'Roy Hale',
+      tagName: 'h1',
+      fontSize: 40,
+      maxWidth: 165,
+      maxHeight: 80,
+    },
+  },
+  topLineText: {
+    content: '<span href="tel:+14155002345">+1 (415) 500-2345</span>',
+    type: 'headingLg',
+    color: 'light-shade-color',
+  },
+  menu: [
+    {
+      id: 'about-us',
+      metadata: {
+        displayName: 'About',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'services',
+      metadata: {
+        displayName: 'Services',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'Resources',
+      metadata: {
+        displayName: 'Resources',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'Speaking',
+      metadata: {
+        displayName: 'Speaking',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'Testimonials',
+      metadata: {
+        displayName: 'Testimonials',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'Blog',
+      metadata: {
+        displayName: 'Blog',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'Contact',
+      metadata: {
+        displayName: 'Contact',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+  ],
+  socialIcons: {
+    networks: [
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: false,
+      preset: 'preset001',
+      offset: 2,
+      color: '#b2b2b2',
+      sizes: [30, 40],
+      size: 36,
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
