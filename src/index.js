d3ae66bb852c1da089dd294822d76ee8a3426400
Vladimir Zelik <kazantip81v@gmail.com>
2018-04-06 02:45:25 +0300

    add style & content

--- src/index.js
+++ src/index.js
@@ -11,6 +11,45 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 28%',
+    },
+    resourceRef: 'Depositphotos_179525000_original.jpg',
+    overlay: {
+      type: 'color',
+      color: 'dark-shade-color',
+      opacity: 0.85,
+    },
+  },
+  title: {
+    content: 'Subscribe Now',
+    type: 'heading',
+  },
+  subtitle: {
+    content: 'Enter your email and get our latest news, special offers, and useful content about our products and services.',
+    type: 'subheading',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        placeholder: 'Email',
+        required: false,
+      },
+    ],
+    submitButton: {
+      title: 'subscribe',
+      textValue: 'subscribe',
+      type: 'primary',
+      size: 'md',
+    },
+    className: 'form',
+    buttonClassName: 'button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
