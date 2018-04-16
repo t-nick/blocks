028c221b77e98936f9cbf1bb8df2dc39c5f04485
nataliadiadkova <nataliadiadkova@gmail.com>
2018-04-16 10:58:21 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,58 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  gallery: [
+    {
+      title: {
+        content: 'Quantum CEO & our partners',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'gallery-img-1.png',
+        alt: 'Quantum CEO & our partners',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+    {
+      title: {
+        content: 'Digital Marketing workshop lecturer',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'gallery-img-2.png',
+        alt: 'Digital Marketing workshop lecturer',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+    {
+      title: {
+        content: 'Quantum Company at WebSummit',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'gallery-img-3.png',
+        alt: 'Quantum Company at WebSummit',
+        galleryId: 'gallery',
+      },
+      caption: {
+        content: 'Design & build',
+        type: 'caption',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: false, label: 'Gallery title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
