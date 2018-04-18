6ded52a7d855459af8235cfe72a055d9cdf737fc
nataliadiadkova <nataliadiadkova@gmail.com>
2018-04-18 14:43:20 +0300

    design

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
   contacts: [
     {
       title: {
@@ -18,15 +19,18 @@ ExtendedWireframe.defaultContent = {
       },
       address: {
         content: '3611 Silas Trail Suite 927<br>Port Yasmeenland, PY 20061 ',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       phone: {
         content: '<a href="tel:2221111111">(222) 111 11 11</a>',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       email: {
         content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       picture: {
         resourceRef: 'location-2.jpg',
@@ -40,15 +44,18 @@ ExtendedWireframe.defaultContent = {
       },
       address: {
         content: '3611 Silas Trail Suite 927<br>Port Yasmeenland, PY 20061',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       phone: {
         content: '<a href="tel:2221111111">(222) 111 11 11</a>',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       email: {
         content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       picture: {
         resourceRef: 'location-1.jpg',
@@ -62,15 +69,18 @@ ExtendedWireframe.defaultContent = {
       },
       address: {
         content: '3611 Silas Trail Suite 927<br>Port Yasmeenland, PY 20061',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       phone: {
         content: '<a href="tel:2221111111">(222) 111 11 11</a>',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       email: {
         content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
-        type: 'text',
+        type: 'subtitle',
+        color: 'dark-shade-color',
       },
       picture: {
         resourceRef: 'location-3.jpg',
@@ -85,6 +95,7 @@ ExtendedWireframe.defaultContent = {
   subtitle: {
     content: 'The French Revolution constituted for the conscience of the dominant aristocratic class<br>a fall from innocence, and upturning of the natural chain of',
     type: 'subtitle',
+    color: 'dark-shade-color',
   },
   'button-1': {
     actionConfig: {
@@ -99,31 +110,14 @@ ExtendedWireframe.defaultContent = {
     },
     textValue: 'Main button (L)',
     type: 'primary',
-  },
-  'button-2': {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Learn more',
-    type: 'secondary',
+    size: 'lg',
   },
 }
 
 ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
   subtitle: {defaultValue: true, label: 'Contacts description', type: 'checkbox'},
-  image: {defaultValue: true, label: 'Location photo', type: 'checkbox'},
-  heading: {defaultValue: true, label: 'Location title', type: 'checkbox'},
-  phone: {defaultValue: true, label: 'Phone', type: 'checkbox'},
-  email: {defaultValue: true, label: 'E-mail', type: 'checkbox'},
   button: {defaultValue: true, label: 'Button', type: 'checkbox'},
-  'button-secondary': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
