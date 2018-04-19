e7cb5312d12d843b97c6359875730e26c85ec051
JulBeet <julbeet@gmail.com>
2018-04-19 19:00:11 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,67 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'dark-shade-color',
+  },
+  subtitle: {
+    content: 'Browse through our photos to see who we are, what drives our work, what inspires us, and pushes to continuous growth to make a difference in the world.',
+    type: 'subheading',
+  },
+  gallery: [{
+    image: {
+      ...Component.defaultContent.gallery[0].image,
+      src: 'image1.jpg',
+    },
+  },
+  {
+    image: {
+      ...Component.defaultContent.gallery[1].image,
+      src: 'image2.jpg',
+    },
+  },
+  {
+    image: {
+      ...Component.defaultContent.gallery[2].image,
+      src: 'image3.jpg',
+    },
+  },
+  {
+    image: {
+      ...Component.defaultContent.gallery[3].image,
+      src: 'image4.jpg',
+    },
+  },
+  {
+    image: {
+      ...Component.defaultContent.gallery[4].image,
+      src: 'image5.jpg',
+    },
+  },
+  {
+    image: {
+      ...Component.defaultContent.gallery[5].image,
+      src: 'image6.jpg',
+    },
+  },
+  {
+    image: {
+      ...Component.defaultContent.gallery[6].image,
+      src: 'image7.jpg',
+    },
+  }],
+  button: {
+    ...Component.defaultContent.button,
+    type: 'secondary',
+    size: 'sm',
+    textValue: 'View More',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Gallery description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
