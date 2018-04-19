e50c19576b901d3bd1b515bc91fea78336233af7
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-04-19 16:26:21 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,62 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  subtitle: {
+    content: 'Every season, there’s always a new must-have (or at least must try) beauty product that comes out — the newest color palette in make-up, the latest in skin care, new scents, etc.',
+    type: 'subtitle',
+  },
+  gallery: [{
+    ...Component.defaultContent.gallery[0],
+    image: {
+      ...Component.defaultContent.gallery[0].image,
+      src: 'img-1.jpg',
+    },
+  },
+  {
+    ...Component.defaultContent.gallery[1],
+    image: {
+      ...Component.defaultContent.gallery[1].image,
+      src: 'img-2.jpg',
+    },
+  },
+  {
+    ...Component.defaultContent.gallery[2],
+    image: {
+      ...Component.defaultContent.gallery[2].image,
+      src: 'img-3.jpg',
+    },
+  },
+  {
+    ...Component.defaultContent.gallery[4],
+    image: {
+      ...Component.defaultContent.gallery[4].image,
+      src: 'img-1.jpg',
+    },
+  },
+  {
+    ...Component.defaultContent.gallery[5],
+    image: {
+      ...Component.defaultContent.gallery[5].image,
+      src: 'img-2.jpg',
+    },
+  },
+  {
+    ...Component.defaultContent.gallery[6],
+    image: {
+      ...Component.defaultContent.gallery[6].image,
+      src: 'img-3.jpg',
+    },
+  }],
+  cta: {
+    textValue: 'View All',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Gallery description', type: 'checkbox'},
+  'secondary-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
