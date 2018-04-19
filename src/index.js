fcb2ab3f2208d7d24496cee8ea1c5518bfb57c39
tangerine69 <mrtangerine69@gmail.com>
2018-04-19 16:25:34 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,112 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: 'Photos',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Jump day <br>September 11, 2017',
+    type: 'subtitle',
+  },
+  gallery: [{
+    image: {
+      resourceRef: 'gallery-image-1.jpg',
+      alt: 'Gallery image',
+      galleryId: 'gallery',
+    },
+    heading: {
+      content: 'Quantum CEO & our partners',
+      type: 'subtitle',
+    },
+  },
+  {
+    image: {
+      resourceRef: 'gallery-image-2.jpg',
+      alt: 'Gallery image',
+      galleryId: 'gallery',
+    },
+    heading: {
+      content: 'Quantum CEO & our partners',
+      type: 'subtitle',
+    },
+  },
+  {
+    image: {
+      resourceRef: 'gallery-image-3.jpg',
+      alt: 'Gallery image',
+      galleryId: 'gallery',
+    },
+    heading: {
+      content: 'Quantum CEO & our partners',
+      type: 'subtitle',
+    },
+  },
+  {
+    image: {
+      resourceRef: 'gallery-image-4.jpg',
+      alt: 'Gallery image',
+      galleryId: 'gallery',
+    },
+    heading: {
+      content: 'Quantum CEO & our partners',
+      type: 'subtitle',
+    },
+  },
+  {
+    image: {
+      resourceRef: 'gallery-image-1.jpg',
+      alt: 'Gallery image',
+      galleryId: 'gallery',
+    },
+    heading: {
+      content: 'Quantum CEO & our partners',
+      type: 'subtitle',
+    },
+  },
+  {
+    image: {
+      resourceRef: 'gallery-image-2.jpg',
+      alt: 'Gallery image',
+      galleryId: 'gallery',
+    },
+    heading: {
+      content: 'Quantum CEO & our partners',
+      type: 'subtitle',
+    },
+  },
+  {
+    image: {
+      resourceRef: 'gallery-image-3.jpg',
+      alt: 'Gallery image',
+      galleryId: 'gallery',
+    },
+    heading: {
+      content: 'Quantum CEO & our partners',
+      type: 'subtitle',
+    },
+  },
+  {
+    image: {
+      resourceRef: 'gallery-image-4.jpg',
+      alt: 'Gallery image',
+      galleryId: 'gallery',
+    },
+    heading: {
+      content: 'Quantum CEO & our partners',
+      type: 'subtitle',
+    },
+  }],
+  cta: {
+    textValue: 'View all photos',
+    type: 'secondary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Gallery description', type: 'checkbox'},
+  'secondary-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
