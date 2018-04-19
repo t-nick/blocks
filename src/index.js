7c75d3c1000bd39353237d445cfcf877854d51ee
KonstantinKH <kkhomenko9@gmail.com>
2018-04-19 19:09:28 +0300

    changed

--- src/index.js
+++ src/index.js
@@ -11,10 +11,41 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  subtitle: {
+    content: 'Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had ',
+    type: 'subtitle',
+  },
+
+  gallery: [
+    {
+      ...Component.defaultContent.gallery[0],
+      image: {
+        ...Component.defaultContent.gallery[0].image,
+        resourceRef: 'img_1.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[1],
+      image: {
+        ...Component.defaultContent.gallery[1].image,
+        resourceRef: 'img_2.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[2],
+      image: {
+        ...Component.defaultContent.gallery[2].image,
+        resourceRef: 'img_3.jpg',
+      },
+    },
+  ],
+
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Gallery description', type: 'checkbox'},
+  'secondary-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
