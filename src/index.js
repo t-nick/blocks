98bf226df6f1b9b7146810ff985c17164cdd7334
isergienko <igorserhiyenko@gmail.com>
2018-04-19 19:08:27 +0300

    done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,54 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Photos of the process',
+  },
+  gallery: [
+    {
+      ...Component.defaultContent.gallery[0],
+      image: {
+        ...Component.defaultContent.gallery[0].image,
+        resourceRef: '1-gallery.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[1],
+      image: {
+        ...Component.defaultContent.gallery[1].image,
+        resourceRef: '2-gallery.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[2],
+      image: {
+        ...Component.defaultContent.gallery[2].image,
+        resourceRef: '3-gallery.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[3],
+      image: {
+        ...Component.defaultContent.gallery[3].image,
+        resourceRef: '4-gallery.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[4],
+      image: {
+        ...Component.defaultContent.gallery[4].image,
+        resourceRef: '2-gallery.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[5],
+      image: {
+        ...Component.defaultContent.gallery[5].image,
+        resourceRef: '4-gallery.png',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
