b961c1920b7e8655ceb427b185df9707cd6a57ff
Vladimir Zelik <kazantip81v@gmail.com>
2018-04-20 11:57:32 +0300

    code review

--- src/index.js
+++ src/index.js
@@ -10,11 +10,7 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  theme: 'white',
-  title: {
-    content: 'About Us',
-    type: 'blockTitle',
-  },
+  ...Component.defaultContent,
   heading: {
     content: 'Heading About Us',
     type: 'heading',
@@ -27,62 +23,11 @@ ExtendedWireframe.defaultContent = {
     content: 'If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is. The European languages are members of the same family. Their separate existence is a myth.',
     type: 'text',
   },
-  'button-1': {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Request a quote',
-    type: 'primary',
-  },
   'button-2': {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
+    ...Component.defaultContent['button-2'],
     textValue: 'Learn more',
     type: 'secondary',
-  },
-  'social-icons': {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'youtube',
-        name: 'YouTube',
-        url: 'http://youtube.com/',
-      },
-    ],
-    target: '_blank',
-    design: {
-      border: 'softRect',
-      innerFill: true,
-      preset: 'preset001',
-      offset: 15,
-      color: '#9b9b9b',
-      sizes: [10, 20, 30, 40],
-      size: 30,
-    },
+    size: 'lg',
   },
   background: {
     type: 'image',
@@ -101,23 +46,9 @@ ExtendedWireframe.defaultContent = {
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  align: {
-    children: [
-      {id: 'left', label: 'Left'},
-      {id: 'center', label: 'Center'},
-      {id: 'right', label: 'Right'},
-    ],
-    defaultValue: 'left',
-    name: 'Aligning',
-    type: 'radio-button-group',
-  },
   heading: {defaultValue: true, label: 'Company title', type: 'checkbox'},
   subheading: {defaultValue: true, label: 'Company description', type: 'checkbox'},
-  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
   'social-icons': {defaultValue: false, label: 'Social media buttons', type: 'checkbox'},
-  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
-  'additional-button': {defaultValue: true, label: 'Additional button', type: 'checkbox'},
-  'top-caption': {defaultValue: false, label: 'Top caption', type: 'hidden'},
 }
 
 export default ExtendedWireframe
