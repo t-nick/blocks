32d6139fbc1e572e58e64d361a9e98e3f16a86dd
nataliadiadkova <nataliadiadkova@gmail.com>
2018-04-20 15:33:05 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,78 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'dark-shade-color',
+  },
+  subtitle: {
+    content: 'They were significantly bigger than him, especially the second man, who frequently slapped him on the shoulder.',
+    type: 'subtitle',
+  },
+  partners: [
+    {
+      logo: {
+        image: {
+          resourceRef: 'logo-img-1.png',
+          width: 78,
+          height: 78,
+        },
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'logo-img-2.png',
+          width: 78,
+          height: 78,
+        },
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'logo-img-3.png',
+          width: 108,
+          height: 37,
+        },
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'logo-img-4.png',
+          width: 73,
+          height: 73,
+        },
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'logo-img-5.png',
+          width: 110,
+          height: 30,
+        },
+      },
+    },
+    {
+      logo: {
+        image: {
+          resourceRef: 'logo-img-6.png',
+          width: 103,
+          height: 36,
+        },
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Partner description', type: 'checkbox'},
+  heading: {defaultValue: false, label: 'Partner title', type: 'checkbox'},
+  body: {defaultValue: false, label: 'About partner', type: 'checkbox'},
+  link: {defaultValue: false, label: 'Partner link', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
