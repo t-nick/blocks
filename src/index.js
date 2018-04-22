76a0d96e569c69c2a792212760663d0a94fa3da7
max-sindi <britva.okkamaa@gmail.com>
2018-04-22 22:08:13 +0300

    design done

--- src/index.js
+++ src/index.js
@@ -11,10 +11,51 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Latest on Instagram',
+  },
+  gallery: [
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        ...Component.defaultContent.gallery[0].picture,
+        resourceRef: 'item-1.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        ...Component.defaultContent.gallery[0].picture,
+        resourceRef: 'item-2.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        ...Component.defaultContent.gallery[0].picture,
+        resourceRef: 'item-3.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        ...Component.defaultContent.gallery[0].picture,
+        resourceRef: 'item-4.png',
+      },
+    },
+  ],
+  cta: {
+    ...Component.defaultContent.cta,
+    textValue: 'Follow',
+    type: 'secondary',
+    size: 'sm',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'secondary-button': {defaultValue: true, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
