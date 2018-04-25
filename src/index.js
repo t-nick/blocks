b36ebced11d8ab47d6908ec72519f7c6e2bd5c13
Sabina <sabina.mamedova90@gmail.com>
2018-04-25 10:05:21 +0300

    add photo

--- src/index.js
+++ src/index.js
@@ -11,10 +11,47 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    ...Component.defaultContent.title,
+    content: '- Gallery -',
+  },
+  subtitle: {
+    ...Component.defaultContent.subtitle,
+    content: 'To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?',
+  },
+  gallery: [
+    {
+      ...Component.defaultContent.gallery[0],
+      image: {
+        ...Component.defaultContent.gallery[0].image,
+        resourceRef: 'raphael-koh-117599.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[1],
+      image: {
+        ...Component.defaultContent.gallery[1].image,
+        resourceRef: 'samuel-zeller-241197.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[2],
+      image: {
+        ...Component.defaultContent.gallery[2].image,
+        resourceRef: 'scott-webb-176434.jpg',
+      },
+    },
+  ],
+  cta: {
+    ...Component.defaultContent.cta,
+    textValue: '- All Images -',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Gallery description', type: 'checkbox'},
+  'secondary-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
