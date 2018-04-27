f6ed153c5d1259b144f99412bdb6058e5c168e38
nataliadiadkova <nataliadiadkova@gmail.com>
2018-04-27 14:11:57 +0300

    design

--- src/index.js
+++ src/index.js
@@ -15,12 +15,9 @@ ExtendedWireframe.defaultContent = {
     content: 'Our Clients',
     type: 'blockTitle',
   },
-  subtitle: {
-    content: 'Over the history of our company, we cooperated with many well-known brands. We are proud of working with each of our partners as they gave us enough support to become one of the leading companies. Here’s a short list of those who have already seen the quality of our services in practice:',
-    type: 'subtitle',
-  },
   partners: [
     {
+      ...Component.defaultContent.partners[0],
       logo: {
         image: {
           resourceRef: '01.png',
@@ -28,30 +25,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Samsung',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Back in 1995, we started cooperation with this brand. Samsung became our main supplier of high-quality tech products. Today, Samsung is our main partner who supports each initiative and actively participates in company development.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[1],
       logo: {
         image: {
           resourceRef: '02.png',
@@ -59,30 +35,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Ford ',
-        type: 'heading',
-      },
-      desc: {
-        content: 'The company that has always put reliability first. We started to work with Ford in 2002, and now this is one of the partners who’ve been with us for more than 15 years! We believe that this partnership is one of the achievements we can be proud of.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[2],
       logo: {
         image: {
           resourceRef: '03.png',
@@ -90,30 +45,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Medical Family',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Health of our team members is a top priority for us. Therefore, we work with the best clinics only, like Medical Family. We entrust our health to them and we entrust our business as well.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[3],
       logo: {
         image: {
           resourceRef: '04.png',
@@ -121,30 +55,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[4],
       logo: {
         image: {
           resourceRef: '05.png',
@@ -152,30 +65,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[5],
       logo: {
         image: {
           resourceRef: '06.png',
@@ -183,30 +75,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[6],
       logo: {
         image: {
           resourceRef: '07.png',
@@ -214,30 +85,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[7],
       logo: {
         image: {
           resourceRef: '08.png',
@@ -245,30 +95,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[8],
       logo: {
         image: {
           resourceRef: '09.png',
@@ -276,30 +105,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[9],
       logo: {
         image: {
           resourceRef: '10.png',
@@ -307,30 +115,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[10],
       logo: {
         image: {
           resourceRef: '11.png',
@@ -338,30 +125,9 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[11],
       logo: {
         image: {
           resourceRef: '12.png',
@@ -369,48 +135,8 @@ ExtendedWireframe.defaultContent = {
           height: 170,
         },
       },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
   ],
-  button: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Learn more',
-    type: 'secondary',
-  },
-  topIcon: {
-    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
-    fill: 'red',
-  },
 }
 
 ExtendedWireframe.modifierScheme = {
