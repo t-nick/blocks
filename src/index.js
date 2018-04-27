786045a955d2edf649c3f80c63c82ae26371f75f
JulBeet <julbeet@gmail.com>
2018-04-27 09:39:16 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   title: {
+    ...Component.defaultContent.title,
     content: 'Join Acrozz Team',
-    type: 'blockTitle',
   },
   text: {
     content: 'Our company is always looking for experienced people who want to become a part of our team. We are truly dedicated to safety, service, and satisfaction.  ',
@@ -24,23 +24,13 @@ ExtendedWireframe.defaultContent = {
     type: 'small',
   },
   'button-1': {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
+    ...Component.defaultContent['button-1'],
     textValue: 'View Positions',
     type: 'secondary',
-    size: 'md',
   },
   picture: {
-    src: 'Depositphotos_47582699_original.jpg',
-    alt: 'Picture about the company',
+    ...Component.defaultContent.picture,
+    resourceRef: 'picture.jpg',
   },
   background: {
     type: 'color',
@@ -51,6 +41,9 @@ ExtendedWireframe.defaultContent = {
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
   'additional-text': {defaultValue: true, label: 'Additional text', type: 'hidden'},
+  'article-picture': {defaultValue: true, label: 'Article picture', type: 'checkbox'},
+  socialIcons: {defaultValue: false, label: 'Social media buttons', type: 'hidden'},
+  subtitle: {defaultValue: false, label: 'Subtitle', type: 'hidden'},
 }
 
 export default ExtendedWireframe
