bc768b2772a2ddda83550bd27a535d3adee2c21c
kirilldonenko <kirill.donenko@gmail.com>
2018-04-28 12:57:25 +0300

    fix spread

--- src/index.js
+++ src/index.js
@@ -13,74 +13,45 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   numbers: [
     {
-      title: {
-        content: 'Increase profits by',
-        type: 'subtitle',
-      },
+      ...Component.defaultContent.numbers[0],
       label: {
+        ...Component.defaultContent.numbers[0].label,
         content: 'Increase in conversion for a redesigned financial services website',
-        type: 'text',
       },
       value: {
+        ...Component.defaultContent.numbers[0].value,
         content: '128',
         type: 'blockTitle',
         color: 'darkShadeColor',
       },
     },
     {
-      title: {
-        content: 'Expand market reach for',
-        type: 'subtitle',
-      },
+      ...Component.defaultContent.numbers[1],
       label: {
+        ...Component.defaultContent.numbers[1].label,
         content: "Boost in sales with a redesigned tile manufacturer's website",
-        type: 'text',
       },
       value: {
+        ...Component.defaultContent.numbers[1].value,
         content: '527',
         type: 'blockTitle',
-        color: 'darkShadeColor',
+        color: '#1e252f',
       },
     },
     {
-      title: {
-        content: 'Improve customer satisfaction for',
-        type: 'subtitle',
-      },
+      ...Component.defaultContent.numbers[2],
       label: {
+        ...Component.defaultContent.numbers[2].label,
         content: "Customer satisfaction rating for an insurance provider's website",
-        type: 'text',
       },
       value: {
+        ...Component.defaultContent.numbers[2].value,
         content: '92',
         type: 'blockTitle',
         color: 'darkShadeColor',
       },
     },
   ],
-  topIcon: {
-    svg:
-      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
-    fill: 'red',
-  },
-  title: {
-    content: 'Our Results in Numbers',
-    type: 'blockTitle',
-  },
-  cta: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Learn more',
-    type: 'secondary',
-  },
 }
 
 ExtendedWireframe.modifierScheme = {
