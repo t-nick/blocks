12bed29910a949fecab7a30cee2061ca5826ad7f
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2018-05-01 16:47:08 +0300

    sdf

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import options from './options.json'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
 
-
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
