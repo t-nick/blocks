1ba09f4e3f5ae29617a25a586e1a3c78bb41f05e
KonstantinKH <kkhomenko9@gmail.com>
2018-05-04 13:57:53 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -11,10 +11,35 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+
+  title: {
+    content: 'Our Values',
+    type: 'blockTitle',
+    color: '#1e1313',
+  },
+  subtitle: {
+    content: 'We are a customer-oriented company, and our  values drive us to achieve more.',
+    type: 'subheading',
+    color: '#646060',
+  },
+  text: {
+    content: 'We always work hard to improve our goods and services to achieve higher results for our clients. We love challenging tasks that require an innovative approach. Thus, if your business needs a unique solution, we are ready to fine-tune our existing products to meet all your demands.',
+    type: 'text',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
