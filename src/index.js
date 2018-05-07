14cacd854dccf42d0033f83999c88749dc2f1924
JulBeet <julbeet@gmail.com>
2018-05-07 18:31:31 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -13,51 +13,45 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   awards: [
     {
-      id: '8130928a-e7c6-4e26-a3ca-a820fa36380e',
-      title: 'R&D Leader in Tech (2017)',
+      ...Component.defaultContent.awards[0],
       picture: {
-        src: '1.png',
-        alt: 'Award illustration photo',
+        ...Component.defaultContent.awards[0].picture,
+        resourceRef: '1.png',
       },
     },
     {
-      id: '057c27bf-4977-4b28-bf35-f4a6db60d5aa',
-      title: 'Most Innovative B2B Solutions (2016)',
+      ...Component.defaultContent.awards[1],
       picture: {
-        src: '2.png',
-        alt: 'Award illustration photo',
+        ...Component.defaultContent.awards[1].picture,
+        resourceRef: '2.png',
       },
     },
     {
-      id: 'c8e93041-896b-4b78-b8d0-37e21a79070f',
-      title: 'Best Customer-Focused Approach on the World Conference (2014)',
+      ...Component.defaultContent.awards[2],
       picture: {
-        src: '3.png',
-        alt: 'Award illustration photo',
+        ...Component.defaultContent.awards[2].picture,
+        resourceRef: '3.png',
       },
     },
     {
-      id: '3ea9fdda-c0e6-418e-9bfe-7bf1b06a534d',
-      title: 'Leader of Innovation (2012)',
+      ...Component.defaultContent.awards[3],
       picture: {
-        src: '4.png',
-        alt: 'Award illustration photo',
+        ...Component.defaultContent.awards[3].picture,
+        resourceRef: '4.png',
       },
     },
     {
-      id: '3ea9fdda-c0e6-418e-9bfe-7bf1b06a534d1',
-      title: 'Leader of Innovation (2012)',
+      ...Component.defaultContent.awards[3],
       picture: {
-        src: '5.png',
-        alt: 'Award illustration photo',
+        ...Component.defaultContent.awards[3].picture,
+        resourceRef: '5.png',
       },
     },
     {
-      id: '3ea9fdda-c0e6-418e-9bfe-7bf1b06a534d2',
-      title: 'Leader of Innovation (2012)',
+      ...Component.defaultContent.awards[3],
       picture: {
-        src: '6.png',
-        alt: 'Award illustration photo',
+        ...Component.defaultContent.awards[3].picture,
+        resourceRef: '6.png',
       },
     },
   ],
@@ -71,15 +65,15 @@ ExtendedWireframe.defaultContent = {
       type: 'cover',
       cover: '50% 50%',
     },
-    resourceRef: 'StockSnap_M0YZ9Q79DZ.jpg',
+    resourceRef: 'picture.jpg',
   },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
-  heading: {defaultValue: false, label: 'Awards title', type: 'checkbox'},
-  subtitle: {defaultValue: false, label: 'Block description', type: 'checkbox'},
+  button: {defaultValue: false, label: 'Button', type: 'hidden'},
+  heading: {defaultValue: false, label: 'Awards title', type: 'hidden'},
+  subtitle: {defaultValue: false, label: 'Block description', type: 'hidden'},
   title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
   'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
