97cd72d8b02c382ea2278444df04ef4c10651369
nataliadiadkova <nataliadiadkova@gmail.com>
2018-05-08 13:29:15 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,64 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg-img.png',
+  },
+  partners: [
+    {
+      ...Component.defaultContent.partners[0],
+      logo: {
+        image: {
+          resourceRef: 'partners-img-1.png',
+          width: 165,
+          height: 68,
+        },
+      },
+    },
+    {
+      ...Component.defaultContent.partners[1],
+      logo: {
+        image: {
+          resourceRef: 'partners-img-2.png',
+          width: 130,
+          height: 40,
+        },
+      },
+    },
+    {
+      ...Component.defaultContent.partners[2],
+      logo: {
+        image: {
+          resourceRef: 'partners-img-3.png',
+          width: 170,
+          height: 40,
+        },
+      },
+    },
+    {
+      ...Component.defaultContent.partners[3],
+      logo: {
+        image: {
+          resourceRef: 'partners-img-4.png',
+          width: 168,
+          height: 40,
+        },
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  heading: {defaultValue: false, label: 'Partner title', type: 'checkbox'},
+  body: {defaultValue: false, label: 'About partner', type: 'checkbox'},
+  link: {defaultValue: false, label: 'Partner link', type: 'checkbox'},
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
