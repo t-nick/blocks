77d90e0d14bdffd93ccebd7586e0802fcdb5a80e
kirilldonenko <kirill.donenko@gmail.com>
2018-05-09 12:21:43 +0300

    com

--- src/index.js
+++ src/index.js
@@ -5,16 +5,43 @@ import style from './style.css'
 
 import options from './options.json'
 
-const {enhancers: {withProps}, hoistStatics} = $editor
+const {
+  enhancers: {withProps},
+  hoistStatics,
+} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  blog: [
+    {
+      ...Component.defaultContent.blog[0],
+      picture: {
+        ...Component.defaultContent.blog[0].picture,
+        resourceRef: 'img-1.png',
+      },
+    },
+    {
+      ...Component.defaultContent.blog[1],
+      picture: {
+        ...Component.defaultContent.blog[1].picture,
+        resourceRef: 'img-2.png',
+      },
+    },
+    {
+      ...Component.defaultContent.blog[2],
+      picture: {
+        ...Component.defaultContent.blog[2].picture,
+        resourceRef: 'img-3.png',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'block-button': {defaultValue: false, label: 'Button', type: 'hidden'},
 }
 
 export default ExtendedWireframe
