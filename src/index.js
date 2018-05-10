a1b9ecfd5670268aed2770812e9b663a5d84f78e
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-05-10 14:49:47 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,38 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'img-bg.jpg',
+  },
+  title: {
+    content: 'What Makes Us Different? Industry Expertise',
+    type: 'blockTitle',
+  },
+  text: {
+    content: 'Our professional team of highly trained web specialists has years of industry experience and are always ready to create masterfully designed sites that can turn any whimsical idea into reality. Let\'s start an enjoyable web journey!',
+    type: 'text',
+  },
+  'button-1': {
+    ...Component.defaultContent['button-1'],
+    textValue: 'start now',
+    type: 'primary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  heading: {defaultValue: false, label: 'Company title', type: 'hidden'},
+  subheading: {defaultValue: false, label: 'Company description', type: 'hidden'},
+  'social-icons': {defaultValue: false, label: 'Social media buttons', type: 'hidden'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
+  'additional-button': {defaultValue: false, label: 'Additional button', type: 'hidden'},
+  'top-caption': {defaultValue: false, label: 'Top caption', type: 'hidden'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
