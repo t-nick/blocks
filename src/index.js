dfdb5b4e617af060ea0f7a23e1d59ae447075eb6
isergienko <igorserhiyenko@gmail.com>
2018-05-11 11:14:54 +0300

    com

--- src/index.js
+++ src/index.js
@@ -11,10 +11,70 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      ...Component.defaultContent.services[0],
+      picture: {
+        ...Component.defaultContent.services[0].picture,
+        resourceRef: '1.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.services[1],
+      picture: {
+        ...Component.defaultContent.services[1].picture,
+        resourceRef: '2.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.services[2],
+      picture: {
+        ...Component.defaultContent.services[2].picture,
+        resourceRef: '3.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.services[3],
+      picture: {
+        ...Component.defaultContent.services[3].picture,
+        resourceRef: '4.jpg',
+      },
+    },
+    {
+      picture: {
+        resourceRef: '5.jpg',
+        alt: 'Service illustration photo',
+      },
+      title: {
+        content: 'Service 1',
+        type: 'heading',
+      },
+      description: {
+        content: 'Multipurpose assistance for online stores and offline retail businesses.',
+        type: 'text',
+      },
+    },
+    {
+      picture: {
+        resourceRef: '6.jpg',
+        alt: 'Service illustration photo',
+      },
+      title: {
+        content: 'Service 1',
+        type: 'heading',
+      },
+      description: {
+        content: 'Multipurpose assistance for online stores and offline retail businesses.',
+        type: 'text',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  link: {defaultValue: false, label: 'Link', type: 'hidden'},
+  'button-secondary': {defaultValue: false, label: 'Secondary button', type: 'hidden'},
 }
 
 export default ExtendedWireframe
