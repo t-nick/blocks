5548e01a827aa1c18b350710b5bf87521a88e024
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-05-11 13:43:50 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,20 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#f8f8f8',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: false, label: 'Testimonials description', type: 'hidden'},
+  image: {defaultValue: false, label: 'Reviewer photo', type: 'hidden'},
+  position: {defaultValue: false, label: 'Reviewer job position', type: 'hidden'},
+  publishDate: {defaultValue: false, label: 'Date of publishing', type: 'hidden'},
+  button: {defaultValue: false, label: 'Secondary Button', type: 'hidden'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
