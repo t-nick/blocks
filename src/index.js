0bdd9b93dd750bfc249c9d8955e95f7400f416ff
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-05-21 15:06:27 +0300

    qa fixes

--- src/index.js
+++ src/index.js
@@ -19,12 +19,14 @@ ExtendedWireframe.defaultContent = {
   text: {
     ...Component.defaultContent.text,
     content: 'For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators. To achieve this, it would be necessary to have uniform grammar, pronunciation and more common words.<br><br> If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental.',
-    color: 'var(--ui-dark-accent-color)',
   },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: false, label: 'Description', type: 'hidden'},
+  heading: {defaultValue: false, label: 'Title', type: 'hidden'},
+  subheading: {defaultValue: false, label: 'Subtitle', type: 'hidden'},
 }
 
 export default ExtendedWireframe
